8a332b4f43eb0c4c7f70578658fda189cf60138c
skovhus <skovhus@users.noreply.github.com>
2026-02-23 12:47:15 +0000

    deploy: 9ed15edadf7f87fbc042afdb340151004d95da80

---
+++ assets/basic-callExpression.input-Cn8BJSC0.js
@@ -0,0 +1,3 @@
+import{j as o,c as p}from"./index-DaPeHGby.js";import{w as r}from"./helpers-DYveQULR.js";const s=t=>o.jsx("div",{...t}),e=p(r(s))`
+  color: red;
+`,m=()=>o.jsx(e,{children:"Hello"});export{m as App};
---
+++ assets/theme-helperBorderByColor.input-CdyQli6P.js
@@ -0,0 +1,3 @@
+import{j as r,c as t}from"./index-DaPeHGby.js";import{m}from"./helpers-DYveQULR.js";const s=t.div`
+  border-bottom: ${o=>m(o.theme.color.bgSub)};
+`,b=()=>r.jsx(s,{});export{b as App};
