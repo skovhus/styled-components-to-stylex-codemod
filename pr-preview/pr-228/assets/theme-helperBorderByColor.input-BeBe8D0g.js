f551de1daac53ee99b0365f1912bad4ca05fb15c
skovhus <skovhus@users.noreply.github.com>
2026-02-24 14:09:03 +0000

    Deploy preview for PR 228 🛫

---
+++ pr-preview/pr-228/assets/theme-helperBorderByColor.input-BeBe8D0g.js
@@ -0,0 +1,3 @@
+import{j as r,c as t}from"./index-IOdGayNZ.js";import{j as s}from"./helpers-zdZ-8NAb.js";const m=t.div`
+  border-bottom: ${o=>s(o.theme.color.bgSub)};
+`,b=()=>r.jsx(m,{});export{b as App};
---
+++ pr-preview/pr-228/assets/theme-prop.input-B60BUUDd.js
@@ -0,0 +1,5 @@
+import{j as i,c as t}from"./index-IOdGayNZ.js";import{a as o}from"./helpers-zdZ-8NAb.js";const e=t.div`
+  height: 100px;
+  width: 100px;
+  ${p=>p.theme.isDark?`padding: ${o()};`:"padding: 100px;"}
+`,x=()=>i.jsx(e,{children:"Theme prop"});export{x as App};
