2d7a1bee07ffe33d1a1dc0b22c18a15f2d0734bf
skovhus <skovhus@users.noreply.github.com>
2026-02-25 18:45:03 +0000

    Deploy preview for PR 240 🛫

---
+++ pr-preview/pr-240/assets/asProp-crossFile.input-CuoRvBLS.js
@@ -0,0 +1,4 @@
+import{j as t,c as e}from"./index-DUg-XDL0.js";import{T as i}from"./text-CesCaowT.js";const r=e(i)`
+  font-size: 24px;
+  font-weight: 600;
+`,a=()=>t.jsx("div",{children:t.jsx(r,{variant:"large",children:"Default Title"})});export{a as App,r as HeaderTitle};
---
+++ pr-preview/pr-240/assets/css-vendorPrefixedPseudo.input-CCwQrQmm.js
@@ -0,0 +1,5 @@
+import{j as t,c as p}from"./index-DUg-XDL0.js";const s=p.input`
+  &::-webkit-slider-thumb {
+    width: 10px;
+  }
+`,i=()=>t.jsx(s,{type:"range"});export{i as App};
