6e2f1d8ea0f1434609d5a18e888a5df04e647bf7
skovhus <skovhus@users.noreply.github.com>
2026-02-26 08:55:29 +0000

    Deploy preview for PR 243 🛫

---
+++ pr-preview/pr-243/assets/attrs-memberExpression.input-Bpga90BQ.js
@@ -0,0 +1,6 @@
+import{j as i,c as o}from"./index-DnaglmpP.js";import{a as t}from"./react-spring-B6A3Ctlo.js";const e=o.div`
+  display: block;
+`,s=o(t.div).attrs({role:"region"})`
+  display: flex;
+  align-items: center;
+`,l=()=>i.jsx(e,{children:i.jsx(s,{children:"Hello"})});export{l as App};
---
+++ pr-preview/pr-243/assets/css-vendorPrefixedProperty.output-DaIJZWAr.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DnaglmpP.js";const p=()=>s.jsx("div",{className:"xo6swyp xjyslct"});export{p as App};
