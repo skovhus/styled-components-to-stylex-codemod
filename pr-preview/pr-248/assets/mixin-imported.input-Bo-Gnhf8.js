15e3777d54734be7328e22e444fea33c4153b566
skovhus <skovhus@users.noreply.github.com>
2026-02-26 10:08:48 +0000

    Deploy preview for PR 248 🛫

---
+++ pr-preview/pr-248/assets/mixin-imported.input-Bo-Gnhf8.js
@@ -0,0 +1,4 @@
+import{j as t,c as i}from"./index-BrfGAQm-.js";import{T as o}from"./helpers-CpXMLPMN.js";const r=i.div`
+  color: red;
+  ${o}
+`,n=()=>t.jsx(r,{children:"Red with imported mixin"});export{n as App};
