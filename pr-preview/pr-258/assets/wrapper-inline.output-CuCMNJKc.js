77c5e815caf84f2fd019bcd3e1b22daff11b2b24
skovhus <skovhus@users.noreply.github.com>
2026-02-26 22:45:29 +0000

    Deploy preview for PR 258 🛫

---
+++ pr-preview/pr-258/assets/wrapper-inline.output-CuCMNJKc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BFsfeWy7.js";import{E as t}from"./external-component-BUE9Tq7Q.js";function n(){return r.jsx("div",{children:r.jsx(t,{isOpen:!0,className:"xdj266r xv8wvd9 xat24cr x1bawvfr"})})}export{n as App};
---
+++ pr-preview/pr-258/assets/wrapper-styleFnSpread.input-BUcJTols.js
@@ -0,0 +1,4 @@
+import{j as r,c as t}from"./index-BFsfeWy7.js";import{E as n}from"./external-component-BUE9Tq7Q.js";const p=t(n)`
+  color: ${o=>o.$color||"gray"};
+  padding: 10px;
+`;function s(o){return r.jsx(p,{...o})}export{s as App};
