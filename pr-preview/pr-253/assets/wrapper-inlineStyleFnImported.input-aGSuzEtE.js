7015fa7f86d7db97af2cc7130a1479404180006f
skovhus <skovhus@users.noreply.github.com>
2026-02-26 13:08:01 +0000

    Deploy preview for PR 253 🛫

---
+++ pr-preview/pr-253/assets/wrapper-inlineStyleFnImported.input-aGSuzEtE.js
@@ -0,0 +1,4 @@
+import{j as r,c as p}from"./index-BQYJcjSO.js";import{E as t}from"./external-component-Blk9Fsox.js";const n=p(t)`
+  color: ${o=>o.$color||"gray"};
+  padding: ${o=>o.$padding||"10px"};
+`;function i(){return r.jsxs("div",{children:[r.jsx(n,{$color:"blue",$padding:"20px",isOpen:!0}),r.jsx(n,{isOpen:!1})]})}export{i as App};
