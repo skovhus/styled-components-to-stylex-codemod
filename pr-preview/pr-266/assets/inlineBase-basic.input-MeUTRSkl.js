e85962f8f948ce896b4ec9771d7393a16162819a
skovhus <skovhus@users.noreply.github.com>
2026-02-27 12:48:08 +0000

    Deploy preview for PR 266 🛫

---
+++ pr-preview/pr-266/assets/inlineBase-basic.input-MeUTRSkl.js
@@ -0,0 +1,5 @@
+import{j as r,c as o}from"./index-D5XBu4ZW.js";import{F as t}from"./inline-base-flex-Bp4SRZj7.js";const n=o(t).attrs({column:!0,gap:16,align:"center"})`
+  padding: 8px;
+  background-color: #f5f5ff;
+  border: 1px solid #667;
+`;function e(){return r.jsx(n,{children:"Basic"})}export{e as App};
---
+++ pr-preview/pr-266/assets/theme-helperBorderByColor.input-CDy64W2G.js
@@ -0,0 +1,3 @@
+import{j as r,c as t}from"./index-D5XBu4ZW.js";import{j as s}from"./helpers-DoXHNIp3.js";const m=t.div`
+  border-bottom: ${o=>s(o.theme.color.bgSub)};
+`,b=()=>r.jsx(m,{});export{b as App};
