6d9561f9b456b74b2b45af109ae9ec6fa574eb55
skovhus <skovhus@users.noreply.github.com>
2026-03-02 15:51:54 +0000

    Deploy preview for PR 273 🛫

---
+++ pr-preview/pr-273/assets/basic-callExpression.output-pSMv8Rat.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CBphIOKd.js";import{w as s}from"./helpers-BcxUCi7h.js";const e=n=>o.jsx("div",{...n}),t=s(e),r=()=>o.jsx(t,{className:"x1e2nbdu",children:"Hello"});export{r as App};
