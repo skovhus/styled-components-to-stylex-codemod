7c9ec62e4ef3c72dd8d17925dfdcb96912676cf5
skovhus <skovhus@users.noreply.github.com>
2026-03-02 08:31:00 +0000

    deploy: 15793e8a35b994b744ebdb006a55b9d304de5ace

---
+++ assets/helper-importNameConflict.input-rFrE2iYs.js
@@ -0,0 +1,3 @@
+import{j as s,c as t}from"./index-CswiYUKj.js";import{h as o}from"./helpers-Cn6IlBK3.js";const i=o("fast"),r=t.div`
+  transition: color ${o("normal")};
+`,a=()=>s.jsxs("div",{children:[s.jsx("div",{children:i}),s.jsx(r,{})]});export{a as App};
