214521d5a640a7523ce12d62f82abf9d4d934f1a
skovhus <skovhus@users.noreply.github.com>
2026-03-02 23:16:49 +0000

    Deploy preview for PR 280 🛫

---
+++ pr-preview/pr-280/assets/useTheme-cssBlock.input-l7ike-VH.js
@@ -0,0 +1,3 @@
+import{j as o,c as s}from"./index-xSKN7mCU.js";import{a as i}from"./helpers-DVeHRyXd.js";const p=s.div`
+  ${t=>t.theme.isDark?"":`padding: ${i()};`}
+`,m=()=>o.jsx(p,{});export{m as App};
