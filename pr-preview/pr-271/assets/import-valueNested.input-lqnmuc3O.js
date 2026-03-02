c01a4d8167b66d6a24cc5612f061f40b56ad2c1d
skovhus <skovhus@users.noreply.github.com>
2026-03-02 13:19:47 +0000

    Deploy preview for PR 271 🛫

---
+++ pr-preview/pr-271/assets/import-valueNested.input-lqnmuc3O.js
@@ -0,0 +1,6 @@
+import{j as r,c as a}from"./index-PKYhFUJi.js";import{k as i}from"./helpers-B7vAOpSq.js";const n=a.div`
+  padding: ${i.ui.spacing.medium};
+  margin: ${i.ui.spacing.small};
+  background: white;
+  border-radius: 8px;
+`;function p(){return r.jsx(n,{})}export{p as App};
