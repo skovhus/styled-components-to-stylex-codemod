5f8debbbf0705344e11bc44e23b68939f8540e18
skovhus <skovhus@users.noreply.github.com>
2026-03-02 08:28:32 +0000

    Deploy preview for PR 270 🛫

---
+++ pr-preview/pr-270/assets/asProp-crossFile.output-BCYNlqIc.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BllQGdXp.js";import{T as a}from"./text-1Qh6WiK3.js";function n(e){const{as:r=a,...s}=e;return t.jsx(r,{...s,className:"x1pvqxga x1s688f"})}const x=()=>t.jsx("div",{children:t.jsx(n,{variant:"large",children:"Default Title"})});export{x as App,n as HeaderTitle};
---
+++ pr-preview/pr-270/assets/basic-callExpression.input-X-dsps2s.js
@@ -0,0 +1,3 @@
+import{j as o,c as p}from"./index-BllQGdXp.js";import{w as r}from"./helpers-CRpee91n.js";const s=t=>o.jsx("div",{...t}),e=p(r(s))`
+  color: red;
+`,m=()=>o.jsx(e,{children:"Hello"});export{m as App};
