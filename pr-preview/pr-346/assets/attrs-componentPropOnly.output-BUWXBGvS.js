d2c275aa781a43dfa54129ab8393671e47fc8c21
skovhus <skovhus@users.noreply.github.com>
2026-03-14 14:21:05 +0000

    Deploy preview for PR 346 🛫

---
+++ pr-preview/pr-346/assets/attrs-componentPropOnly.output-BUWXBGvS.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BLnv-6EX.js";import{T as r}from"./text-DNAtZ5_7.js";const e=()=>t.jsx("div",{style:{padding:"16px"},children:t.jsx(r,{variant:"title2",children:"Hello World"})});export{e as App};
---
+++ pr-preview/pr-346/assets/theme-helperBorderByColor.input-DL-w_EGG.js
@@ -0,0 +1,3 @@
+import{j as r,c as t}from"./index-BLnv-6EX.js";import{m}from"./helpers-DY0hVCez.js";const s=t.div`
+  border-bottom: ${o=>m(o.theme.color.bgSub)};
+`,b=()=>r.jsx(s,{});export{b as App};
