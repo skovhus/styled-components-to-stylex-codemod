4a3fd521bdb1b81cd34a2f38d7d3bc381d335f08
skovhus <skovhus@users.noreply.github.com>
2026-03-14 10:16:23 +0000

    Deploy preview for PR 342 🛫

---
+++ pr-preview/pr-342/assets/text-CVvwiZWy.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-Gq3wqmKq.js";function s(r){const{variant:o,color:a,...t}=r;return e.createElement("span",t)}export{s as T};
---
+++ pr-preview/pr-342/assets/theme-helperBorderByColor.input-Biz36OV3.js
@@ -0,0 +1,3 @@
+import{j as r,c as t}from"./index-Gq3wqmKq.js";import{m}from"./helpers-B0jiPghH.js";const s=t.div`
+  border-bottom: ${o=>m(o.theme.color.bgSub)};
+`,b=()=>r.jsx(s,{});export{b as App};
