4857f90e1797a3f050730a59042a2c411f9a69ab
skovhus <skovhus@users.noreply.github.com>
2026-03-16 19:23:02 +0000

    deploy: 774fb5c3c8cb5842bc481792d0b903c9481263ed

---
+++ assets/css-gradient.input-CrEBPo8k.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-D5hx4oJ2.js";import{d as n}from"./helpers-0o6FizW3.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
---
+++ assets/helper-importNameConflict.input-DWmdjcdV.js
@@ -0,0 +1,3 @@
+import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-D5hx4oJ2.js";import{S as i}from"./helpers-0o6FizW3.js";e(t(),1);var a=n(),o=i(`fast`),s=r.div`
+  transition: color ${i(`normal`)};
+`,c=()=>(0,a.jsxs)(`div`,{children:[(0,a.jsx)(`div`,{children:o}),(0,a.jsx)(s,{})]});export{c as App};
