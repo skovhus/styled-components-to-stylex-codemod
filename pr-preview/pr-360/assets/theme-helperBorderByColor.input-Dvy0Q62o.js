1191dd8ad1fb2e60d3446f73e7533339b0874c74
skovhus <skovhus@users.noreply.github.com>
2026-03-22 19:09:26 +0000

    Deploy preview for PR 360 🛫

---
+++ pr-preview/pr-360/assets/theme-helperBorderByColor.input-Dvy0Q62o.js
@@ -0,0 +1,3 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-BCjZefjU.js";import{i as n}from"./helpers-B8N03MM2.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
