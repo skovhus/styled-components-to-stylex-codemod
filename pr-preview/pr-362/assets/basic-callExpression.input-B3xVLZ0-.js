4879a9f89288076eb2e0503763791ba4341b8222
skovhus <skovhus@users.noreply.github.com>
2026-03-22 22:49:33 +0000

    Deploy preview for PR 362 🛫

---
+++ pr-preview/pr-362/assets/basic-callExpression.input-B3xVLZ0-.js
@@ -0,0 +1,3 @@
+import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-B6Edtzrt.js";import{E as i}from"./helpers-CrrT1rp3.js";e(t(),1);var a=n(),o=r(i(e=>(0,a.jsx)(`div`,{...e})))`
+  color: red;
+`,s=()=>(0,a.jsx)(o,{children:`Hello`});export{s as App};
--- pr-preview/pr-362/assets/mixin-extraClassNames.input-DCkIy4_M.js
+++
@@ -1,4 +0,0 @@
-import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-DpzH6t4H.js";import{s as n}from"./helpers-DTBYhKg2.js";var r=e(),i=t.div`
-  pointer-events: all;
-  ${n(!0)};
-`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
