2a0ad5a0468262f550302ee868727e9e502ac9ba
skovhus <skovhus@users.noreply.github.com>
2026-03-22 23:22:25 +0000

    Deploy preview for PR 362 🛫

---
+++ pr-preview/pr-362/assets/css-gradient.input-Dr3KBFV7.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-DRUBaCAK.js";import{f as n}from"./helpers-Ct97Xu_s.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
