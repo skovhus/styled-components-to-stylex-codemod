d714baf395ef8f11eaa7f72b8ae619caf8a66f8f
skovhus <skovhus@users.noreply.github.com>
2026-03-23 12:16:18 +0000

    deploy: f0bd149add06b7a48c9aa89a5bc378af8277a015

---
+++ assets/theme-helperBorderByColor.input-CQ9XLbDx.js
@@ -0,0 +1,3 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-Wraq7WSN.js";import{i as n}from"./helpers-DNpxQGoH.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
