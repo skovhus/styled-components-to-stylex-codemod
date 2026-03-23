b9ce4add49469c59547581ec7bdf71b2c24a21d0
skovhus <skovhus@users.noreply.github.com>
2026-03-23 10:31:21 +0000

    Deploy preview for PR 363 🛫

---
+++ pr-preview/pr-363/assets/mixin-extraClassNames.input-r6BhxQm-.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-CkRWwx-i.js";import{s as n}from"./helpers-1s_75i9J.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
