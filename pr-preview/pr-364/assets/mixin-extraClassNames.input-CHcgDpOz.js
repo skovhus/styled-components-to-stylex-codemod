9921205b446785384cc4229265980aa588c36882
skovhus <skovhus@users.noreply.github.com>
2026-03-23 15:59:15 +0000

    Deploy preview for PR 364 🛫

---
+++ pr-preview/pr-364/assets/mixin-extraClassNames.input-CHcgDpOz.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-Doru_INP.js";import{s as n}from"./helpers-DXQPeMPd.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
---
+++ pr-preview/pr-364/assets/theme-helperBorderByColor.input-DH7zyolB.js
@@ -0,0 +1,3 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-Doru_INP.js";import{i as n}from"./helpers-DXQPeMPd.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
