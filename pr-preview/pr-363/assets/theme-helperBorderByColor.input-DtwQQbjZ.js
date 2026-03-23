044c1c65a8db14e25934111fd70600264ec1f3b1
skovhus <skovhus@users.noreply.github.com>
2026-03-23 11:04:10 +0000

    Deploy preview for PR 363 🛫

--- pr-preview/pr-363/assets/theme-helperBorderByColor.input-DtwQQbjZ.js
+++
@@ -1,3 +0,0 @@
-import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-D6x8cl9d.js";import{i as n}from"./helpers-Q-Zhgnj4.js";var r=e(),i=t.div`
-  border-bottom: ${e=>n(e.theme.color.bgSub)};
-`,a=()=>(0,r.jsx)(i,{});export{a as App};
