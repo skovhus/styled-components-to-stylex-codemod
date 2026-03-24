c74dd2e6e4671b81985bada3e18bacbea095b404
skovhus <skovhus@users.noreply.github.com>
2026-03-24 11:31:49 +0000

    Deploy preview for PR 365 🛫

--- pr-preview/pr-365/assets/attrs-memberExpression.input-6bUGuja7.js
+++
@@ -1,6 +0,0 @@
-import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-PnoOrfbC.js";import{t as n}from"./react-spring-BRel8jIB.js";var r=e(),i=t.div`
-  display: block;
-`,a=t(n.div).attrs({role:`region`})`
-  display: flex;
-  align-items: center;
-`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
