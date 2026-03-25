61c7381a8af965b60c784c1331c7ca3ad7a56c4a
skovhus <skovhus@users.noreply.github.com>
2026-03-25 22:17:17 +0000

    Deploy preview for PR 371 🛫

---
+++ pr-preview/pr-371/assets/conditional-ternaryTemplateHelperCall.input-Dz9VcriT.js
@@ -0,0 +1,6 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-DBBCzHe5.js";import{a as n}from"./helpers-n1nS1E2G.js";var r=e(),i=t.div`
+  flex: 1;
+  width: 100px;
+  height: 100px;
+  background: ${e=>e.$faded?`linear-gradient(to bottom, ${n(`bgSub`)(e)} 70%, rgba(0, 0, 0, 0) 100%)`:`linear-gradient(to bottom, ${n(`bgSub`)(e)} 70%, ${n(`bgSub`)(e)} 100%)`};
+`,a=()=>(0,r.jsx)(i,{$faded:!0});export{a as App};
---
+++ pr-preview/pr-371/assets/helper-callShadowing.input-B5oxAtue.js
@@ -0,0 +1,3 @@
+import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-DBBCzHe5.js";import"./helpers-n1nS1E2G.js";e(t(),1);var i=n();function a(){return r.div`
+    background-color: ${(e=>`#${e}`)(`ff0000`)};
+  `}var o=()=>(0,i.jsx)(a(),{children:`Colored text`});export{o as App};
