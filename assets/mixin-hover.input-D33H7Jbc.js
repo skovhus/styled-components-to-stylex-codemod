0d19bb281ebe46ab9ba3496cbcfafddc69ced798
skovhus <skovhus@users.noreply.github.com>
2026-03-25 23:29:32 +0000

    deploy: edf7aa966e441eb1d4820aedc04d0eae13df3e49

---
+++ assets/mixin-hover.input-D33H7Jbc.js
@@ -0,0 +1,8 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t,u as n}from"./index-CWd5nope.js";var r=e(),i=n`
+  &:hover {
+    color: blue;
+  }
+`,a=t.button`
+  color: red;
+  ${i}
+`,o=()=>(0,r.jsx)(a,{children:`Hover me`});export{o as App};
