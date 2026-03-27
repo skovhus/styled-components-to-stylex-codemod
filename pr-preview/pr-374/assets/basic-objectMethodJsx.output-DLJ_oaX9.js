c8b5a16191c4f21e03dd42b9f5c63071a726a6f8
skovhus <skovhus@users.noreply.github.com>
2026-03-27 09:29:30 +0000

    Deploy preview for PR 374 🛫

---
+++ pr-preview/pr-374/assets/basic-objectMethodJsx.output-DLJ_oaX9.js
@@ -0,0 +1,1 @@
+import"./stylex-B9_aYcJU.js";import{f as e}from"./index-CGgyz-wQ.js";var t=e(),n={render(){return(0,t.jsx)(`button`,{className:`xxb0df0 xe8ttls`,children:`Click me`})}},r=()=>n.render();export{r as App};
---
+++ pr-preview/pr-374/assets/inlineBase-exportedNoLocalJsx.input-B3P4W1jQ.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-CGgyz-wQ.js";import{t as n}from"./inline-base-flex-X7z0w2lg.js";var r=e(),i=t(n).attrs({column:!0,gap:16})`
+  padding: 4px;
+  background-color: #f4f4ff;
+`;function a(){return(0,r.jsx)(`div`,{children:`Exported only`})}export{a as App,i as Container};
