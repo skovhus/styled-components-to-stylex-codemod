500c84d3da6d44ebbd3673c2343583311e1774ab
skovhus <skovhus@users.noreply.github.com>
2026-03-27 11:30:13 +0000

    deploy: 89a264e426a2ea8d4dcb7ae0a6f258c1824c4234

---
+++ assets/wrapper-styleFnSpread.input-BksjgdQJ.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-pOSVzVrj.js";import{t as n}from"./external-component-wTkvv7xx.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
