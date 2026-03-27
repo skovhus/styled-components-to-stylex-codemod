1b0d5f1430adfac6cab04fa817bb3cc1746e3fbe
skovhus <skovhus@users.noreply.github.com>
2026-03-27 06:43:21 +0000

    Deploy preview for PR 374 🛫

---
+++ pr-preview/pr-374/assets/cssHelper-conditionalTernaryThemeIndex.input-CbxZ0cpE.js
@@ -0,0 +1,9 @@
+import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r,u as i}from"./index-Cin0tPK2.js";e(t(),1);var a=n(),o=r.div`
+  display: flex;
+  ${e=>e.outlined?i`
+          outline: 1px solid
+            ${e.color?e.theme.color[e.color]:e.theme.color.labelMuted};
+        `:i`
+          background: ${e.color?e.theme.color[e.color]:e.theme.color.labelMuted};
+        `}
+`,s=()=>(0,a.jsxs)(`div`,{style:{display:`flex`,gap:12,flexDirection:`column`},children:[(0,a.jsx)(o,{outlined:!0,children:`Outlined default`}),(0,a.jsx)(o,{outlined:!0,color:`labelBase`,children:`Outlined custom`}),(0,a.jsx)(o,{outlined:!1,children:`Background default`}),(0,a.jsx)(o,{outlined:!1,color:`labelBase`,children:`Background custom`})]});export{s as App};
