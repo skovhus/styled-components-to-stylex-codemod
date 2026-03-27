df71035a3f2d992946e14e2693048c7811d6fc78
skovhus <skovhus@users.noreply.github.com>
2026-03-27 07:46:14 +0000

    Deploy preview for PR 374 🛫

--- pr-preview/pr-374/assets/basic-callExpression.input-DPItOF9I.js
+++
@@ -1,3 +0,0 @@
-import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-Cin0tPK2.js";import{E as i}from"./helpers-Dlg9nmpH.js";e(t(),1);var a=n(),o=r(i(e=>(0,a.jsx)(`div`,{...e})))`
-  color: red;
-`,s=()=>(0,a.jsx)(o,{children:`Hello`});export{s as App};
--- pr-preview/pr-374/assets/theme-helperBorderByColor.input-CkiVAemb.js
+++
@@ -1,3 +0,0 @@
-import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-Cin0tPK2.js";import{i as n}from"./helpers-Dlg9nmpH.js";var r=e(),i=t.div`
-  border-bottom: ${e=>n(e.theme.color.bgSub)};
-`,a=()=>(0,r.jsx)(i,{});export{a as App};
