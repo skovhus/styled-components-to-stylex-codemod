ec7a53391cf164b1aae287aa96ec1f0977c43fd3
skovhus <skovhus@users.noreply.github.com>
2026-03-27 10:19:49 +0000

    Deploy preview for PR 376 🛫

--- pr-preview/pr-376/assets/helper-callShadowing.input-CVsOpA-e.js
+++
@@ -1,3 +0,0 @@
-import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-BIKmzUa5.js";import"./helpers-D2wp1XsB.js";e(t(),1);var i=n();function a(){return r.div`
-    background-color: ${(e=>`#${e}`)(`ff0000`)};
-  `}var o=()=>(0,i.jsx)(a(),{children:`Colored text`});export{o as App};
---
+++ pr-preview/pr-376/assets/theme-themeMethodCall.input-epVQ3sCI.js
@@ -0,0 +1,5 @@
+import{o as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-D4cBbUL-.js";import{f as n,s as r}from"./index-C77dy0j8.js";e(t(),1);var i=n(),a=r.div`
+  padding: 16px;
+  background-color: ${e=>e.theme.highlightVariant(e.theme.color.bgBorderSolid)};
+  color: #333;
+`;function o(){return(0,i.jsx)(`div`,{style:{display:`flex`,gap:16,padding:16},children:(0,i.jsx)(a,{children:`Highlight box`})})}export{o as App};
