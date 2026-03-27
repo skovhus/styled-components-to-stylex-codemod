ac0a81f0f75b59c73f50127b651058382fc54c48
skovhus <skovhus@users.noreply.github.com>
2026-03-27 09:23:39 +0000

    deploy: 9760baac6e388a9ea680bb9ae33fc92b1f89e7af

---
+++ assets/attrs-memberExpression.output-Dp9i1W1l.js
@@ -0,0 +1,1 @@
+import"./react-D4cBbUL-.js";import"./stylex-B9_aYcJU.js";import{f as e}from"./index-BY6E3DHd.js";import{t}from"./react-spring-CMgVcu-A.js";var n=e(),r=()=>(0,n.jsx)(`div`,{className:`x1lliihq`,children:(0,n.jsx)(t.div,{role:`region`,className:`x78zum5 x6s0dn4`,children:`Hello`})});export{r as App};
---
+++ assets/interpolation-pseudoConditional.input-Do34WcX0.js
@@ -0,0 +1,17 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-BY6E3DHd.js";import{p as n}from"./helpers-BEH0G5nB.js";import{t as r}from"./TouchDeviceToggle-B6FMyEU4.js";var i=e(),a=t.button`
+  color: blue;
+  padding: 8px 16px;
+
+  &:${n} {
+    color: red;
+    background-color: yellow;
+  }
+`,o=t.button`
+  color: green;
+  padding: 8px 16px;
+
+  &&:${n} {
+    color: purple;
+    background-color: orange;
+  }
+`,s=()=>(0,i.jsx)(r,{children:()=>(0,i.jsxs)(`div`,{style:{display:`flex`,gap:`16px`,padding:`16px`},children:[(0,i.jsx)(a,{children:`Highlight Button`}),(0,i.jsx)(o,{children:`Specific Button`})]})});export{s as App};
