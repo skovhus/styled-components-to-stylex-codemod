03067f8634947fcae82617974d08482de7c66e99
skovhus <skovhus@users.noreply.github.com>
2026-04-06 12:51:30 +0000

    Deploy preview for PR 377 🛫

---
+++ pr-preview/pr-377/assets/css-gradient.input-DVUdx-ES.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-BLDqzuo4.js";import{f as n}from"./helpers-CVJjFcXt.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
