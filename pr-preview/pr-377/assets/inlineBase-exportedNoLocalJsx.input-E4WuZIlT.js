d2f683cf9e6c6be0db7c730ed86f7d9c691a1237
skovhus <skovhus@users.noreply.github.com>
2026-04-07 12:33:08 +0000

    Deploy preview for PR 377 🛫

---
+++ pr-preview/pr-377/assets/inlineBase-exportedNoLocalJsx.input-E4WuZIlT.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-D-s1CKya.js";import{t as n}from"./inline-base-flex-CpvLn66h.js";var r=e(),i=t(n).attrs({column:!0,gap:16})`
+  padding: 4px;
+  background-color: #f4f4ff;
+`;function a(){return(0,r.jsx)(`div`,{children:`Exported only`})}export{a as App,i as Container};
---
+++ pr-preview/pr-377/assets/theme-helperBorderByColor.input-CRiG1dPf.js
@@ -0,0 +1,3 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-D-s1CKya.js";import{i as n}from"./helpers-DQMsyCJP.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
