3c8104ddfbc63c3d6f8a04de3d9d080410cfd60c
skovhus <skovhus@users.noreply.github.com>
2026-04-07 06:39:56 +0000

    Deploy preview for PR 382 🛫

---
+++ pr-preview/pr-382/assets/wrapper-styleFnSpread.input-CqhyrcuN.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-CnYL50zI.js";import{t as n}from"./external-component-DdDrm4vU.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
