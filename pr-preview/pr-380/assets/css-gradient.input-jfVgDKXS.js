bc6a7fe6103f6c28075221a08f6c2ad551187429
skovhus <skovhus@users.noreply.github.com>
2026-04-07 06:05:43 +0000

    Deploy preview for PR 380 🛫

---
+++ pr-preview/pr-380/assets/css-gradient.input-jfVgDKXS.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-DTwcEKcR.js";import{f as n}from"./helpers-BX9j4Ane.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
---
+++ pr-preview/pr-380/assets/interpolation-pseudoConditional.input-DFnTrMzc.js
@@ -0,0 +1,17 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-DTwcEKcR.js";import{p as n}from"./helpers-BX9j4Ane.js";import{t as r}from"./TouchDeviceToggle-CqFk_mEz.js";var i=e(),a=t.button`
+  color: blue;
+  padding: 8px 16px;
+
+  &:${n} {
+    color: red;
+    background-color: yellow;
+  }
+`,o=t.button`
+  color: green;
+  padding: 8px 16px;
+
+  &&:${n} {
+    color: purple;
+    background-color: orange;
+  }
+`,s=()=>(0,i.jsx)(r,{children:()=>(0,i.jsxs)(`div`,{style:{display:`flex`,gap:`16px`,padding:`16px`},children:[(0,i.jsx)(a,{children:`Highlight Button`}),(0,i.jsx)(o,{children:`Specific Button`})]})});export{s as App};
