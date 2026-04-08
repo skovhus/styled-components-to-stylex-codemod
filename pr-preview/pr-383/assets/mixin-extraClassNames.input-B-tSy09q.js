492765943094abeeff7892ca067d3b5daad2c47d
skovhus <skovhus@users.noreply.github.com>
2026-04-08 07:22:58 +0000

    Deploy preview for PR 383 🛫

---
+++ pr-preview/pr-383/assets/mixin-extraClassNames.input-B-tSy09q.js
@@ -0,0 +1,4 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-rd0PUPYk.js";import{s as n}from"./helpers-CEq9YFai.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
---
+++ pr-preview/pr-383/assets/theme-helperBorderByColor.input-B5Nk7uDY.js
@@ -0,0 +1,3 @@
+import"./react-D4cBbUL-.js";import{f as e,s as t}from"./index-rd0PUPYk.js";import{i as n}from"./helpers-CEq9YFai.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
