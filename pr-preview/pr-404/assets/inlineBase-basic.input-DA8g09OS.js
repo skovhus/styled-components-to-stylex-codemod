928c621ce45109aeb41d0bb469367e084555e2a8
skovhus <skovhus@users.noreply.github.com>
2026-04-27 09:59:16 +0000

    Deploy preview for PR 404 🛫

---
+++ pr-preview/pr-404/assets/inlineBase-basic.input-DA8g09OS.js
@@ -0,0 +1,5 @@
+import{f as e,s as t}from"./index-6njy6uFT.js";import{t as n}from"./inline-base-flex-Be2Nwx6N.js";var r=e(),i=t(n).attrs({column:!0,gap:16,align:`center`})`
+  padding: 8px;
+  background-color: #f5f5ff;
+  border: 1px solid #667;
+`;function a(){return(0,r.jsx)(i,{children:`Basic`})}export{a as App};
---
+++ pr-preview/pr-404/assets/inlineBase-singleVariant.input-Pqnxb4qU.js
@@ -0,0 +1,4 @@
+import{f as e,s as t}from"./index-6njy6uFT.js";import{t as n}from"./inline-base-flex-Be2Nwx6N.js";var r=e(),i=t.div`
+  padding: 16px;
+  background-color: #f0f5ff;
+`;function a(){return(0,r.jsxs)(i,{children:[(0,r.jsx)(n,{column:!0,gap:24,center:!0,children:`Content A`}),(0,r.jsx)(n,{children:`Content B`})]})}export{a as App};
