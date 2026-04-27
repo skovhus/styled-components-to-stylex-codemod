ada22d0a87b37fdbdfd3348a558d2c2c0f8a0832
skovhus <skovhus@users.noreply.github.com>
2026-04-27 08:53:22 +0000

    Deploy preview for PR 402 🛫

---
+++ pr-preview/pr-402/assets/theme-helperBorderByColor.input-CKygSksJ.js
@@ -0,0 +1,3 @@
+import{f as e,s as t}from"./index-B9t9vlp9.js";import{i as n}from"./helpers-yVHIIpeC.js";var r=e(),i=t.div`
+  border-bottom: ${e=>n(e.theme.color.bgSub)};
+`,a=()=>(0,r.jsx)(i,{});export{a as App};
