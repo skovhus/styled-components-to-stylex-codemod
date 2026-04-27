eaf21302d8c8706526a1955de826483b9e32afef
skovhus <skovhus@users.noreply.github.com>
2026-04-27 17:15:09 +0000

    Deploy preview for PR 407 🛫

---
+++ pr-preview/pr-407/assets/helper-adapterCalleeResolution.input-BhKNRb_p.js
@@ -0,0 +1,10 @@
+import"./chunk-jRWAZmH_.js";import{f as e,p as t,s as n}from"./index-Ci83gWZW.js";import{C as r,l as i,u as a}from"./helpers-B8UiWD2Q.js";t();var o=e(),s=n.span`
+  font-weight: ${a(`medium`)};
+  font-size: ${i(`medium`)};
+  transition: color ${r(`fast`)};
+`,c=n.button`
+  font-weight: ${a(`bold`)};
+  font-size: ${i(`small`)};
+  transition: background ${r(`normal`)};
+  padding: 8px 16px;
+`;function l({children:e}){return(0,o.jsx)(s,{children:e})}function u({children:e}){return(0,o.jsx)(c,{children:e})}var d=()=>(0,o.jsxs)(`div`,{children:[(0,o.jsx)(l,{children:`Hello World`}),(0,o.jsx)(u,{children:`Click Me`})]});export{d as App,u as Button,l as Text};
