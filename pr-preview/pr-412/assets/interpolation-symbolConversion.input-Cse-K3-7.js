4465a7439308ff5a30a1a8d439e349196919525a
skovhus <skovhus@users.noreply.github.com>
2026-04-28 22:24:43 +0000

    Deploy preview for PR 412 🛫

---
+++ pr-preview/pr-412/assets/interpolation-symbolConversion.input-Cse-K3-7.js
@@ -0,0 +1,5 @@
+import{f as e,s as t}from"./index-mBEUdLZC.js";import{S as n}from"./helpers-CtytFi7m.js";var r=e(),i=t.div`
+  padding: ${e=>e.$compact?`${n()}`:`16px`};
+  margin-left: ${e=>e.$compact?`calc(-4px + ${n()})`:`0px`};
+  background: #f0f0f0;
+`,a=()=>(0,r.jsxs)(`div`,{children:[(0,r.jsx)(i,{$compact:!0,children:`Compact mode`}),(0,r.jsx)(i,{$compact:!1,children:`Normal mode`})]});export{a as App};
