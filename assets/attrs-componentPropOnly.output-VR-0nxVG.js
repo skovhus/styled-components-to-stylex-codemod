e14a912e354fb0423eeea69f1930febb0f39265a
skovhus <skovhus@users.noreply.github.com>
2026-04-29 12:26:20 +0000

    deploy: a664b169938e8f9fd303b9cbffb78390bbad4b7e

---
+++ assets/attrs-componentPropOnly.output-VR-0nxVG.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-DGaVFZxc.js";import{t}from"./text-CMibkLJl.js";var n=e(),r=()=>(0,n.jsx)(`div`,{style:{padding:`16px`},children:(0,n.jsx)(t,{variant:`title2`,children:`Hello World`})});export{r as App};
---
+++ assets/selector-crossFileReverse.input-BM-cfByN.js
@@ -0,0 +1,11 @@
+import{f as e,s as t}from"./index-DGaVFZxc.js";import{n}from"./cross-file-icon.styled-Co8MK7bL.js";var r=e(),i=t.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-color: gray;
+  transition: background-color 0.25s;
+
+  ${n}:hover & {
+    background-color: rebeccapurple;
+  }
+`;function a(){return(0,r.jsx)(`div`,{style:{padding:16},children:(0,r.jsxs)(n,{href:`#`,children:[(0,r.jsx)(i,{}),`Hover me`]})})}export{a as App};
