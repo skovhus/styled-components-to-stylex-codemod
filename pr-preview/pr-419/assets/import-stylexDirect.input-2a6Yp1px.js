a4549acf8dc9bcbacc5af1d01952297d840628a4
skovhus <skovhus@users.noreply.github.com>
2026-05-04 20:15:50 +0000

    Deploy preview for PR 419 🛫

---
+++ pr-preview/pr-419/assets/import-stylexDirect.input-2a6Yp1px.js
@@ -0,0 +1,6 @@
+import"./chunk-jRWAZmH_.js";import{f as e,p as t,r as n,s as r}from"./index-Bpzex4bT.js";t();var i=e(),a=r.div`
+  border-radius: 8px;
+  padding: 6px;
+  position: fixed;
+  z-index: ${n.modal};
+`;function o(){return(0,i.jsx)(a,{children:`StyleX Direct Import`})}export{o as App};
---
+++ pr-preview/pr-419/assets/import-valueBinaryExpression.input-CiPTiNfX.js
@@ -0,0 +1,5 @@
+import"./chunk-jRWAZmH_.js";import{f as e,p as t,s as n}from"./index-Bpzex4bT.js";import{O as r}from"./helpers-zJXMJWMR.js";t();var i=e(),a=n.div`
+  z-index: ${r.dialog+1};
+  padding: 16px;
+  background-color: white;
+`;function o(){return(0,i.jsx)(a,{children:`Dialog Content`})}export{o as App};
