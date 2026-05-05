c551da136ef12fc7b253803e572d70f29578fff5
skovhus <skovhus@users.noreply.github.com>
2026-05-05 11:04:58 +0000

    Deploy preview for PR 422 🛫

---
+++ pr-preview/pr-422/assets/css-borderHelper.input-CczjiPAp.js
@@ -0,0 +1,8 @@
+import"./chunk-jRWAZmH_.js";import{f as e,p as t,s as n}from"./index-BgZ_AwUK.js";import{S as r,x as i}from"./helpers-DAKaR84g.js";t();var a=e(),o=n.div`
+  border-left: ${i(`labelMuted`)};
+`,s=n.div`
+  border: ${i(`labelMuted`)};
+`,c=n.div`
+  border: ${r(`transparent`)};
+  padding: 8px 16px;
+`,l=()=>(0,a.jsxs)(`div`,{style:{padding:`10px`},children:[(0,a.jsx)(o,{children:`Bordered left`}),(0,a.jsx)(s,{children:`Bordered box`}),(0,a.jsx)(c,{children:`Thin border`})]});export{l as App};
