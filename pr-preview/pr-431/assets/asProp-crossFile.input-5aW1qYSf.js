caafa1fe33fb4f03ac9f39a5fa4305ee76f48084
skovhus <skovhus@users.noreply.github.com>
2026-05-05 11:24:04 +0000

    Deploy preview for PR 431 🛫

---
+++ pr-preview/pr-431/assets/asProp-crossFile.input-5aW1qYSf.js
@@ -0,0 +1,4 @@
+import{f as e,s as t}from"./index-BO10cZMW.js";import{t as n}from"./text-CQ4AtkW3.js";var r=e(),i=t(n)`
+  font-size: 24px;
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
---
+++ pr-preview/pr-431/assets/attrs-componentPropOnly.output-DoG6UT_S.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-BO10cZMW.js";import{t}from"./text-CQ4AtkW3.js";var n=e(),r=()=>(0,n.jsx)(`div`,{style:{padding:`16px`},children:(0,n.jsx)(t,{variant:`title2`,children:`Hello World`})});export{r as App};
