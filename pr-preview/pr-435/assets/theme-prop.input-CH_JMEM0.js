69d058b3eeacb9ef076b64cd5de9f4c1ede8ec25
skovhus <skovhus@users.noreply.github.com>
2026-05-05 11:55:41 +0000

    Deploy preview for PR 435 🛫

---
+++ pr-preview/pr-435/assets/theme-prop.input-CH_JMEM0.js
@@ -0,0 +1,9 @@
+import{f as e,s as t}from"./index-DXoZNO1L.js";import{C as n}from"./helpers-0AK9bbQ-.js";var r=e(),i=t.div`
+  height: 100px;
+  width: 100px;
+  ${e=>e.theme.isDark?`padding: ${n()};`:`padding: 100px;`}
+`,a=t.div`
+  height: 100px;
+  width: 100px;
+  ${e=>e.theme.mode===`dark`?`color: white;`:`color: black;`}
+`,o=()=>(0,r.jsxs)(`div`,{children:[(0,r.jsx)(i,{children:`Theme prop`}),(0,r.jsx)(a,{children:`Theme mode`})]});export{o as App};
