e7226eeff8eec6b00df9bff6859e7d3b12e93b36
skovhus <skovhus@users.noreply.github.com>
2026-05-05 18:41:26 +0000

    deploy: ee8693b9269dc286dabd2c2830d71e0c68d12ae4

---
+++ assets/attrs-chained.input-BeyKMobt.js
@@ -0,0 +1,5 @@
+import"./chunk-jRWAZmH_.js";import{f as e,p as t,s as n}from"./index-FzLU-rZV.js";t();var r=e();function i(e){let{as:t=`span`,children:n,className:i,style:a}=e;return(0,r.jsx)(t,{className:i,style:a,children:n})}var a=n(n(i).attrs({as:`button`})`
+  cursor: pointer;
+`)`
+  color: blue;
+`,o=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(a,{children:`Click me`})});export{o as App,a as ClickableText};
---
+++ assets/attrs-componentPropOnly.input-C5U0OSJP.js
@@ -0,0 +1,1 @@
+import{f as e,s as t}from"./index-FzLU-rZV.js";import{t as n}from"./text-DxHDClSI.js";var r=e(),i=t(n).attrs({variant:`title2`})``,a=()=>(0,r.jsx)(`div`,{style:{padding:`16px`},children:(0,r.jsx)(i,{children:`Hello World`})});export{a as App};
