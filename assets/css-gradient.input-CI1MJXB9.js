030c71491a870cdf54a083b348f755e3152d73c4
skovhus <skovhus@users.noreply.github.com>
2026-05-06 11:07:52 +0000

    deploy: 93086b4c43858b9b51665755a968879b05b55ccc

---
+++ assets/css-gradient.input-CI1MJXB9.js
@@ -0,0 +1,4 @@
+import{c as e,p as t}from"./index-DbDHYQIX.js";import{p as n}from"./helpers-B9lW1kCV.js";var r=t(),i=e.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
