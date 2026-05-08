7e6f19146adf9c8e2ee06b6aa8155c71032fc7e7
skovhus <skovhus@users.noreply.github.com>
2026-05-08 11:50:01 +0000

    deploy: 70d3cfa30c93190d731d3ec8746811720ead5c31

---
+++ assets/extending-overrideUsedParent.input-D06z9y6J.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-DyNLQtlH.js";import{t as r}from"./text-CGfdyYIe.js";n();var i=e(),a=t(r)`
+  margin-left: 8px;
+  color: blue;
+`,o=t(a)`
+  margin-left: 4px;
+`,s=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(a,{children:`Direct use of parent (margin-left: 8px)`}),(0,i.jsx)(o,{children:`Child overrides margin-left to 4px`})]});export{s as App,o as HelpText};
