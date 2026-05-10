6131161fb2b78e7200f177c280bcd7435bf5bc47
skovhus <skovhus@users.noreply.github.com>
2026-05-10 20:03:14 +0000

    Deploy preview for PR 467 🛫

---
+++ pr-preview/pr-467/assets/css-gradient.input-DVN_JX8i.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-jDvsXdNQ.js";import{m as n}from"./helpers-DK4YmAL2.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
