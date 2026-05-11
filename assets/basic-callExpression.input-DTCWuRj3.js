1f8c8da65cc1cfc3d124651f42302319b40069f6
skovhus <skovhus@users.noreply.github.com>
2026-05-11 12:02:21 +0000

    deploy: a4042fa347d7fcd9830b6351340a4776b23fafda

---
+++ assets/basic-callExpression.input-DTCWuRj3.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-DZMxl7PN.js";import{A as r}from"./helpers-B9WA7HPM.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
---
+++ assets/wrapper-inlineStyleFnImported.input-05x176B-.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-DZMxl7PN.js";import{t as n}from"./external-component-eig0SZVC.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: ${e=>e.$padding||`10px`};
+`;function a(){return(0,r.jsxs)(`div`,{children:[(0,r.jsx)(i,{$color:`blue`,$padding:`20px`,isOpen:!0}),(0,r.jsx)(i,{isOpen:!1})]})}export{a as App};
