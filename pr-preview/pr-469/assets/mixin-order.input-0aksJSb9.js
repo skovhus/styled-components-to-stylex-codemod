f5746d8e3f5d956feff4e3cb8845c07fbe314c64
skovhus <skovhus@users.noreply.github.com>
2026-05-11 11:19:37 +0000

    Deploy preview for PR 469 🛫

---
+++ pr-preview/pr-469/assets/mixin-order.input-0aksJSb9.js
@@ -0,0 +1,11 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,d as n}from"./index-hYRsbUyM.js";import{r}from"./helpers-CBOtm-3D.js";var i=e(),a=n`
+  font-weight: bold;
+`,o=t.div`
+  color: red;
+  ${a}
+  ${r}
+`,s=t.div`
+  color: blue;
+  ${r}
+  ${a}
+`,c=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(o,{children:`Local then imported`}),(0,i.jsx)(s,{children:`Imported then local`})]});export{c as App};
