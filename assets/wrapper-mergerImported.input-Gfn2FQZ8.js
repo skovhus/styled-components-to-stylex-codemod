306c736461cc6a6fd9d5988e1e895475750fe038
skovhus <skovhus@users.noreply.github.com>
2026-05-11 06:52:54 +0000

    deploy: 2fcc7d069a155f21c2bc48af97e95c347a9f82c7

---
+++ assets/wrapper-mergerImported.input-Gfn2FQZ8.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-GtR_FcXo.js";import{t as r}from"./loading-CVEH3qFo.js";n();var i=e(),a=t(r)`
+  height: 100%;
+  flex-direction: column;
+  gap: 8px;
+  flex: 1;
+`,o=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(a,{size:`large`,text:`Loading settings…`}),(0,i.jsx)(a,{size:`small`,text:!1})]});export{o as App,a as StyledLoading};
