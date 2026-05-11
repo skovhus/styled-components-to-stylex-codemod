2da35081a39a2da227e272afc852604770c53c68
skovhus <skovhus@users.noreply.github.com>
2026-05-11 11:25:40 +0000

    deploy: 17e9f9ba3f7d8143c499cc62be49dff6cf58e938

---
+++ assets/basic-singleUseStyleProp.input-lN9m0cL5.js
@@ -0,0 +1,7 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-DyxnPoWU.js";var n=e(),r=4,i=t.div`
+  margin: 3px;
+  background-color: coral;
+`,a=t.span`
+  font-weight: bold;
+  color: navy;
+`,o=()=>(0,n.jsxs)(`div`,{style:{display:`flex`,gap:8},children:[(0,n.jsx)(i,{style:{left:6+r},children:`Tick`}),(0,n.jsx)(a,{className:`custom-label`,children:`Label`})]});export{o as App};
