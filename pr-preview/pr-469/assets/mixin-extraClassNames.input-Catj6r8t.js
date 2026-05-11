3a2a9f28c47827504eed96ee8ae9363db1a431f2
skovhus <skovhus@users.noreply.github.com>
2026-05-11 10:58:10 +0000

    Deploy preview for PR 469 🛫

---
+++ pr-preview/pr-469/assets/mixin-extraClassNames.input-Catj6r8t.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-DIxJQMfz.js";import{s as n}from"./helpers-D8_Sv9Ke.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
