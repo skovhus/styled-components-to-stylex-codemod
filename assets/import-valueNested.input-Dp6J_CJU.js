93d710faa2c9eec5521e273f6627511674f55a8e
skovhus <skovhus@users.noreply.github.com>
2026-05-11 06:49:35 +0000

    deploy: 39e2884af5c3aca48998c9d0834869e741e59204

---
+++ assets/import-valueNested.input-Dp6J_CJU.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-B6N4j0mb.js";import{o as r}from"./helpers-C_OL-WgP.js";n();var i=e(),a=t.div`
+  padding: ${r.ui.spacing.medium};
+  margin: ${r.ui.spacing.small};
+  background: white;
+  border-radius: 8px;
+`;function o(){return(0,i.jsx)(a,{})}export{o as App};
