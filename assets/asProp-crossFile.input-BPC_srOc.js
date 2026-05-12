5d28eb79e15f4f7859bc145e4b7135dbdbc533f7
skovhus <skovhus@users.noreply.github.com>
2026-05-12 08:26:48 +0000

    deploy: 082d955bc76a95239321305ba1b0d33f190cba22

---
+++ assets/asProp-crossFile.input-BPC_srOc.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-BQeglijx.js";import{t as n}from"./text-CG9rhQ_d.js";var r=e(),i=t(n)`
+  font-size: 24px;
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
