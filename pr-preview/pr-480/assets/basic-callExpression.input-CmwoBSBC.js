eddd5a3833e03c4afc18715fc6b3792fd8ad5a28
skovhus <skovhus@users.noreply.github.com>
2026-05-13 21:54:26 +0000

    Deploy preview for PR 480 🛫

---
+++ pr-preview/pr-480/assets/basic-callExpression.input-CmwoBSBC.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-B83wAgEL.js";import{A as r}from"./helpers-CwbtGuQy.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
