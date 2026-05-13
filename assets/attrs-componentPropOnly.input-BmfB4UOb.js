f6799ea63ea8b383e03756a71b85f861e4a423b8
skovhus <skovhus@users.noreply.github.com>
2026-05-13 06:45:17 +0000

    deploy: bb44d58583f8609cb39582160f274aec706802d1

---
+++ assets/attrs-componentPropOnly.input-BmfB4UOb.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-1e13UpLR.js";import{t as n}from"./text-C8wZQvj2.js";var r=e(),i=t(n).attrs({variant:`title2`})``,a=()=>(0,r.jsx)(`div`,{style:{padding:`16px`},children:(0,r.jsx)(i,{children:`Hello World`})});export{a as App};
---
+++ assets/helper-importNameConflict.output-BnEMLXaY.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-1e13UpLR.js";import{D as n}from"./helpers-DSA1Fqy4.js";t();var r=e(),i=n(`fast`),a=()=>(0,r.jsxs)(`div`,{children:[(0,r.jsx)(`div`,{children:i}),(0,r.jsx)(`div`,{className:`x4lopbt`})]});export{a as App};
