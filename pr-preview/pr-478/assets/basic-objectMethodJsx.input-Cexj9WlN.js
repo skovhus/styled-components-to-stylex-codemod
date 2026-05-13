9e85b9d4f8b2c17e4dc33d830a27e4790db81d22
skovhus <skovhus@users.noreply.github.com>
2026-05-13 07:23:38 +0000

    Deploy preview for PR 478 🛫

---
+++ pr-preview/pr-478/assets/basic-objectMethodJsx.input-Cexj9WlN.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-CMM1R4pz.js";var n=e(),r=t.button`
+  background: ${`#BF4F74`};
+  padding: 8px;
+`,i={render(){return(0,n.jsx)(r,{children:`Click me`})}},a=()=>i.render();export{a as App};
