e3d3bc1b7d93791715239ef399e90df788de54cc
skovhus <skovhus@users.noreply.github.com>
2026-05-13 08:29:12 +0000

    Deploy preview for PR 476 🛫

---
+++ pr-preview/pr-476/assets/helper-destructuredThemeArg.input-w7GHKGbt.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-D0CCMohe.js";import{i as n}from"./helpers-CK3BW5rl.js";var r=e(),i=t.div`
+  border-bottom: ${({theme:e})=>n(e.color.bgSub)};
+  padding: 8px 16px;
+`,a=()=>(0,r.jsx)(`div`,{style:{display:`flex`,gap:16,padding:16},children:(0,r.jsx)(i,{children:`Box with border`})});export{a as App};
