bf715d58162cba5030fc1d49fa6b62ac43abd28c
skovhus <skovhus@users.noreply.github.com>
2026-05-18 10:19:00 +0000

    deploy: 232e2013dc70bebb3338fbe2d06e3c421eb9f087

---
+++ assets/attrs-asComponentProps.input-KOuBfNo1.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-BP0PPxV3.js";import{t as n}from"./flex-BNz7F8Mt.js";var r=e(),i=t(t.div``).attrs({as:n,column:!0})`
+  background: white;
+  border-radius: 4px;
+`,a=()=>(0,r.jsxs)(i,{children:[(0,r.jsx)(`div`,{children:`Item 1`}),(0,r.jsx)(`div`,{children:`Item 2`})]});export{a as App};
---
+++ assets/wrapper-styleFnDynamicCallSite.input-Bihw-mcb.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-BP0PPxV3.js";import{t as n}from"./flex-BNz7F8Mt.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
