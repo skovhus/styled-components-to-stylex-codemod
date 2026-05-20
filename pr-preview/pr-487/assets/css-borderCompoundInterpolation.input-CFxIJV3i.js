ecfed49b41cea4d758fc297458a5b09904244efb
skovhus <skovhus@users.noreply.github.com>
2026-05-20 12:39:25 +0000

    Deploy preview for PR 487 🛫

--- pr-preview/pr-487/assets/css-borderCompoundInterpolation.input-CFxIJV3i.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-B94dWM66.js";import{E as r,a as i}from"./helpers-DDTlM_h4.js";n();var a=e(),o=t.div`
-  border-radius: 2px;
-  padding: 2px 6px;
-  border: ${r()} solid ${i(`bgBorderFaint`)};
-  flex-shrink: 0;
-`;function s(){return(0,a.jsx)(o,{children:`Hello`})}export{s as App};
---
+++ pr-preview/pr-487/assets/css-gradient.input-Bpyr-9Lc.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-uLtPpJQw.js";import{m as n}from"./helpers-CZo5cijx.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
