c6032b80891c81c4378047288a05611b1439415a
skovhus <skovhus@users.noreply.github.com>
2026-05-20 13:16:52 +0000

    Deploy preview for PR 487 🛫

---
+++ pr-preview/pr-487/assets/css-gradient.input-CPS1fadC.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-BR6jMRYW.js";import{m as n}from"./helpers-C7yu3IEI.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
