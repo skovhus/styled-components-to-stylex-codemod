36ea51f0942dacaaf545077ee533282613ce0942
skovhus <skovhus@users.noreply.github.com>
2026-05-20 11:06:41 +0000

    Deploy preview for PR 487 🛫

---
+++ pr-preview/pr-487/assets/attrs-componentPropOnly.input-D_Ri4Zv_.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-Cwv0yn-7.js";import{t as n}from"./text-D5r3EPOG.js";var r=e(),i=t(n).attrs({variant:`title2`})``,a=()=>(0,r.jsx)(`div`,{style:{padding:`16px`},children:(0,r.jsx)(i,{children:`Hello World`})});export{a as App};
