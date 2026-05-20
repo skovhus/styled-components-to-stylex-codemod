f0ac9780f4282ed04c0d85c3849e77d254f4c3ef
skovhus <skovhus@users.noreply.github.com>
2026-05-20 12:58:45 +0000

    Deploy preview for PR 487 🛫

---
+++ pr-preview/pr-487/assets/partial-customComponentRoot.output-B1gu9tQz.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-NuKhgRiI.js";import{t as r}from"./text-CYok9QpJ.js";n();var i=e(),a=t(r)`
+  color: #1d4ed8;
+  font-weight: 600;
+`,o=()=>(0,i.jsx)(`div`,{className:`xe8ttls xmftpkk`,children:(0,i.jsx)(a,{variant:`small`,children:`Imported custom root`})});export{o as App};
