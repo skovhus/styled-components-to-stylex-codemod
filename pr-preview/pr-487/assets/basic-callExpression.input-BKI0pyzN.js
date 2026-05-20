410e27f6ed194f34bfffa3553b19030c1de4584d
skovhus <skovhus@users.noreply.github.com>
2026-05-20 15:34:39 +0000

    Deploy preview for PR 487 🛫

---
+++ pr-preview/pr-487/assets/basic-callExpression.input-BKI0pyzN.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-DaAtMpal.js";import{A as r}from"./helpers-CR6zrR_P.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
