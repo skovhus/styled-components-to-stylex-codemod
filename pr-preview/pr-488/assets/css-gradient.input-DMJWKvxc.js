5d1ead6832667066d3063f73f92f4b7353e9cb1a
skovhus <skovhus@users.noreply.github.com>
2026-05-21 14:36:20 +0000

    Deploy preview for PR 488 🛫

---
+++ pr-preview/pr-488/assets/css-gradient.input-DMJWKvxc.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-EYvCkOpW.js";import{m as n}from"./helpers-BnW-C80O.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
--- pr-preview/pr-488/assets/theme-helperBorderByColor.input-D2pg3lU7.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-D6LzhpWS.js";import{i as n}from"./helpers-zOwqblLO.js";var r=e(),i=t.div`
-  padding: 12px;
-  border-bottom: ${e=>n(e.theme.color.bgSub)};
-`,a=()=>(0,r.jsx)(i,{children:`Themed helper border`});export{a as App};
