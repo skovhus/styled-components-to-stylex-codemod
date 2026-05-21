6c5048f8d8a0b8666083351c6adcc866f946239c
skovhus <skovhus@users.noreply.github.com>
2026-05-21 13:57:36 +0000

    Deploy preview for PR 488 🛫

---
+++ pr-preview/pr-488/assets/attrs-memberExpression.input-DUcjQyRF.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-DZ2H8a0R.js";import{t as n}from"./react-spring-CCZ2pfHU.js";var r=e(),i=t.div`
+  display: block;
+`,a=t(n.div).attrs({role:`region`})`
+  display: flex;
+  align-items: center;
+`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
