52b2e4e9401eecb906d890cde7440cd3b2fbba57
skovhus <skovhus@users.noreply.github.com>
2026-05-23 21:47:12 +0000

    Deploy preview for PR 489 🛫

---
+++ pr-preview/pr-489/assets/css-gradient.input-BMMehj56.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-QtPhiHiA.js";import{m as n}from"./helpers-DLaUUN7u.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
