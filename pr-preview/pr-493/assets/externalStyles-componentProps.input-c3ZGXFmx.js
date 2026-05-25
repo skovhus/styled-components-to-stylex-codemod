c24d28e78a7cbe807a199fff40063ee97106881f
skovhus <skovhus@users.noreply.github.com>
2026-05-25 22:31:25 +0000

    Deploy preview for PR 493 🛫

---
+++ pr-preview/pr-493/assets/externalStyles-componentProps.input-c3ZGXFmx.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-COHxmW3S.js";n();var r=e();function i(e){return(0,r.jsx)(`div`,{children:e.content})}var a=t(i)`
+  padding: 24px 0 48px;
+`,o=()=>(0,r.jsx)(a,{content:`hello`});export{o as App};
