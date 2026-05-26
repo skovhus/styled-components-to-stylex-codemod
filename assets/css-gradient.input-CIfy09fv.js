387fe3c214c01fb95306b1fd32b00c3f883b87b7
skovhus <skovhus@users.noreply.github.com>
2026-05-26 20:30:24 +0000

    deploy: fad4bda80d3371003a4d56cd028ed8f10325f951

---
+++ assets/css-gradient.input-CIfy09fv.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DPLVk1vF.js";import{m as n}from"./helpers-D5Sk222F.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
---
+++ assets/wrapper-inline.output-Bl48gn-B.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-DPLVk1vF.js";import{t as n}from"./external-component-BowM4pw7.js";t();var r=e();function i(){return(0,r.jsx)(`div`,{children:(0,r.jsx)(n,{isOpen:!0,className:`xdj266r xv8wvd9 xat24cr x1bawvfr`})})}export{i as App};
