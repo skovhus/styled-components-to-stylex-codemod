a0b6344c0b44ebfa6ee16f22898a201d7497b6b0
skovhus <skovhus@users.noreply.github.com>
2026-05-26 19:31:36 +0000

    Deploy preview for PR 499 🛫

---
+++ pr-preview/pr-499/assets/wrapper-styleFnSpread.input-65T8zz4T.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-x1lN72h9.js";import{t as n}from"./external-component-P9PiC4i_.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
