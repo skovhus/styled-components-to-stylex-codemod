70b2e0caf6691193ed8b69a8035ad7284b9f0285
skovhus <skovhus@users.noreply.github.com>
2026-05-26 09:05:01 +0000

    Deploy preview for PR 496 🛫

---
+++ pr-preview/pr-496/assets/css-gradient.input-PGX4zkkF.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-C-rvm4IU.js";import{m as n}from"./helpers-DLYH5dJh.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
