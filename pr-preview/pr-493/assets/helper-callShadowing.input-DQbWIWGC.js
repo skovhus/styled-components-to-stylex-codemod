a307f0e93c069b8782003dcd179793fa6b77cf4a
skovhus <skovhus@users.noreply.github.com>
2026-05-26 07:30:24 +0000

    Deploy preview for PR 493 🛫

---
+++ pr-preview/pr-493/assets/helper-callShadowing.input-DQbWIWGC.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,p as n}from"./index-B4n1ObPk.js";import"./helpers-Dunz-T3F.js";n();var r=e();function i(){return t.div`
+    background-color: ${(e=>`#${e}`)(`ff0000`)};
+  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
---
+++ pr-preview/pr-493/assets/wrapper-dynamicSxExtension.input-iGhEOBDz.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-B4n1ObPk.js";import{t as n}from"./sx-aware-component-BO4OF2NE.js";var r=e(),i=t(n)`
+  padding-left: ${e=>`${e.electronYTrafficLightPosition??0}px`};
+`,a=t(i)`
+  margin-left: 4px;
+`,o=()=>(0,r.jsx)(a,{electronYTrafficLightPosition:12,children:`Account`});export{o as App,i as TopContainer};
