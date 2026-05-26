ab5b64adfb4c5c0609f952423f7d7dd1d64a02c3
skovhus <skovhus@users.noreply.github.com>
2026-05-26 09:03:59 +0000

    Deploy preview for PR 495 🛫

---
+++ pr-preview/pr-495/assets/wrapper-styleFnSpread.input-CKbpslGj.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-DIqbeRvW.js";import{t as n}from"./external-component-BIj-e8fu.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
