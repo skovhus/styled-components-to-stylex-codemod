d91ed2f2a86b76f2a37a0c7ee174017c995531f1
skovhus <skovhus@users.noreply.github.com>
2026-05-26 21:03:50 +0000

    Deploy preview for PR 499 🛫

---
+++ pr-preview/pr-499/assets/inlineBase-templateOverride.input-C8USCBZn.js
@@ -0,0 +1,7 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BmfaL-8s.js";import{t as n}from"./inline-base-flex-BnmGR7gf.js";var r=e(),i=t(n).attrs({column:!0})`
+  display: grid;
+  flex-direction: row;
+  gap: 4px;
+  padding: 8px;
+  background-color: #eef;
+`;function a(){return(0,r.jsx)(i,{children:`Override`})}export{a as App};
--- pr-preview/pr-499/assets/mediaQuery-custom.input-DdlglH18.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-7cl1IDHt.js";var n=e(),r=t.div`
-  @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
-    font-size: ${e=>e.$size-5}px;
-  }
-`,i=()=>(0,n.jsx)(r,{$size:16,children:`Hello`});export{i as App,r as ConditionalContainer};
