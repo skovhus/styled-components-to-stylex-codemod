5b3e7d43a9297c6828e9c51b4fd50992c8b72af3
skovhus <skovhus@users.noreply.github.com>
2026-05-26 07:19:26 +0000

    deploy: 6a9e0ffd56fa943018a75a3cb06fc931fe3b2e76

---
+++ assets/attrs-componentPropOnly.input-1sHpdLHW.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t}from"./index-zcdN2jIt.js";import{t as n}from"./text-Co8BLf9n.js";var r=e(),i=t(n).attrs({variant:`title2`})``,a=()=>(0,r.jsx)(`div`,{style:{padding:`16px`},children:(0,r.jsx)(i,{children:`Hello World`})});export{a as App};
