bdeff340da70c917b56d2f5b6156116a68703600
skovhus <skovhus@users.noreply.github.com>
2026-05-26 20:45:28 +0000

    Deploy preview for PR 501 🛫

---
+++ pr-preview/pr-501/assets/wrapper-inline.input-D-ZK6fcJ.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-N8BQXGDO.js";import{t as r}from"./external-component-DuldA029.js";n();var i=e(),a=t(r)`
+  margin: 0 -8px 0 -8px;
+`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
