f4fbecc694654390395896647969379fc8f7b03e
skovhus <skovhus@users.noreply.github.com>
2026-05-28 10:45:23 +0000

    Deploy preview for PR 504 🛫

---
+++ pr-preview/pr-504/assets/attrs-memberExpression.input-svsBtBOC.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DN9jWtl0.js";import{t as n}from"./react-spring-BBU_W0kA.js";var r=e(),i=t.div`
+  display: block;
+`,a=t(n.div).attrs({role:`region`})`
+  display: flex;
+  align-items: center;
+`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
--- pr-preview/pr-504/assets/basic-callExpression.input-IPrjmLkL.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-pEdsxNhJ.js";import{A as r}from"./helpers-BpW6AUVP.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
-  color: red;
-`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
