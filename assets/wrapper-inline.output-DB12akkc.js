a1a4a73fb6f9b231ae1bd4c6056bbae072ea50d8
skovhus <skovhus@users.noreply.github.com>
2026-05-28 21:06:22 +0000

    deploy: 55ac2bf7e77e2282209f88ce44f6af3e777b058f

---
+++ assets/wrapper-inline.output-DB12akkc.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-DZw6vMKQ.js";import{t as n}from"./external-component-BN4yQmHq.js";t();var r=e();function i(){return(0,r.jsx)(`div`,{children:(0,r.jsx)(n,{isOpen:!0,className:`xdj266r xv8wvd9 xat24cr x1bawvfr`})})}export{i as App};
