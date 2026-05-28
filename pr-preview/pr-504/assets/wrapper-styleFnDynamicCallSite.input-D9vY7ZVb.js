2860e44bc9ced609da03427de774a7098bdde12b
skovhus <skovhus@users.noreply.github.com>
2026-05-28 08:14:08 +0000

    Deploy preview for PR 504 🛫

---
+++ pr-preview/pr-504/assets/wrapper-styleFnDynamicCallSite.input-D9vY7ZVb.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DXnPoeNO.js";import{t as n}from"./flex-C6sjMV2e.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
