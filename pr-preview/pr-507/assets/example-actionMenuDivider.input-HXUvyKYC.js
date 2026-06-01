4260000556dfa2043581a6fd2c13ab8e176504d6
skovhus <skovhus@users.noreply.github.com>
2026-06-01 07:35:40 +0000

    Deploy preview for PR 507 🛫

---
+++ pr-preview/pr-507/assets/example-actionMenuDivider.input-HXUvyKYC.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-DRhAmcFI.js";import{t as r}from"./inline-base-flex-H2IHumff.js";n();var i=e(),a=t(r)`
+  user-select: none;
+  height: 30px;
+  padding: 4px 12px 0px 14px;
+  align-items: center;
+`;function o(e){return(0,i.jsx)(a,{noMinWidth:!0,className:e.className,style:e.style,children:(0,i.jsx)(`span`,{children:e.text})})}var s=()=>(0,i.jsx)(o,{text:`Section`});export{s as App};
---
+++ pr-preview/pr-507/assets/inlineBase-dynamicConsumedProp.input-Jz9ZhiSb.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-DRhAmcFI.js";import{t as r}from"./inline-base-flex-H2IHumff.js";n();var i=e(),a=t.div`
+  padding: 16px;
+  background-color: #f0f5ff;
+`;function o({isCompact:e}){return(0,i.jsx)(a,{children:(0,i.jsx)(r,{column:!0,grow:1,align:e?`start`:`center`,gap:e?8:16,children:`Content`})})}export{o as App};
