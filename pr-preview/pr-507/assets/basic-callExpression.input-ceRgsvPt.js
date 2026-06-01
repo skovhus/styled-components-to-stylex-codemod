6c5da7a1d927d18c02c98c3e49dec9dc56b2ee78
skovhus <skovhus@users.noreply.github.com>
2026-06-01 09:51:38 +0000

    Deploy preview for PR 507 🛫

---
+++ pr-preview/pr-507/assets/basic-callExpression.input-ceRgsvPt.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-BbCWSlwN.js";import{A as r}from"./helpers-87xljZ4C.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
