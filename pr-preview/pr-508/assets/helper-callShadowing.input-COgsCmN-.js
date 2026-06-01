4471107e366bb6060e5227c64e1f652e8ffc78a0
skovhus <skovhus@users.noreply.github.com>
2026-06-01 21:13:23 +0000

    Deploy preview for PR 508 🛫

---
+++ pr-preview/pr-508/assets/helper-callShadowing.input-COgsCmN-.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-B0Lj3XEI.js";import"./helpers-B5L64KHa.js";n();var r=e();function i(){return t.div`
+    background-color: ${(e=>`#${e}`)(`ff0000`)};
+  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
---
+++ pr-preview/pr-508/assets/wrapper-styleFnDynamicCallSite.input-D1d1fq7a.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-B0Lj3XEI.js";import{t as n}from"./flex-D3lyqwoU.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
