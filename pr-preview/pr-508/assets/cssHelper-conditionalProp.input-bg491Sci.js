a582ed1b64546564297e66abfc42c00d7b0f69c0
skovhus <skovhus@users.noreply.github.com>
2026-06-01 21:17:51 +0000

    Deploy preview for PR 508 🛫

--- pr-preview/pr-508/assets/cssHelper-conditionalProp.input-bg491Sci.js
+++
@@ -1,7 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{c as t,l as n}from"./index-B0Lj3XEI.js";var r=e();function i(e){let{gap:t,className:n,style:i,children:a}=e;return(0,r.jsx)(`div`,{className:n,style:{display:`flex`,gap:t,...i},children:a})}var a=n(i)`
-  ${e=>e.$color&&t`
-      background-color: ${e.$color};
-    `}
-  padding: 2px 6px;
-  border-radius: 3px;
-`,o=()=>(0,r.jsx)(a,{gap:4,$color:`rebeccapurple`,children:`Hello`});export{o as App,a as Container};
--- pr-preview/pr-508/assets/wrapper-styleFnMergeIntoBase.input-6zkU7mgU.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-B0Lj3XEI.js";var n=e(),r=t.div`
-  z-index: ${e=>e.$zIndex};
-  position: fixed;
-  pointer-events: ${e=>e.$disablePointerEvents?`none`:`auto`};
-`;function i(){return(0,n.jsx)(r,{$zIndex:100,$disablePointerEvents:!1,children:`content`})}export{i as App};
