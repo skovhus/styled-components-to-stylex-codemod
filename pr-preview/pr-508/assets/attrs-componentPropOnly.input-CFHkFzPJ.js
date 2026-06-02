cdd662a5b857e82eba7518fcd4eb67f960c0138b
skovhus <skovhus@users.noreply.github.com>
2026-06-02 12:02:45 +0000

    Deploy preview for PR 508 🛫

--- pr-preview/pr-508/assets/attrs-componentPropOnly.input-CFHkFzPJ.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DX9eJl_F.js";import{t as n}from"./text-CgDKc0YF.js";import{t as r}from"./flex-BcRrT_F4.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
-  width: 100%;
-  margin-top: 16px;
-`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
