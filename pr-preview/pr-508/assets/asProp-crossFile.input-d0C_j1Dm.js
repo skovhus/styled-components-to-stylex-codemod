6e959e27d092cf8f7c100449939dd592b40fdc9a
skovhus <skovhus@users.noreply.github.com>
2026-06-02 15:28:43 +0000

    Deploy preview for PR 508 🛫

---
+++ pr-preview/pr-508/assets/asProp-crossFile.input-d0C_j1Dm.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-CZoed9Zj.js";import{t as n}from"./text-BWBzuHEo.js";var r=e(),i=t(n)`
+  font-size: 24px;
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
