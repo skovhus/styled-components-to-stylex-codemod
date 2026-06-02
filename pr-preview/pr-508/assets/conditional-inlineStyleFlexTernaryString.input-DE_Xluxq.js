f64e5ece2d3841fab07da7117a289695d22e9dd1
skovhus <skovhus@users.noreply.github.com>
2026-06-02 11:46:52 +0000

    Deploy preview for PR 508 🛫

---
+++ pr-preview/pr-508/assets/conditional-inlineStyleFlexTernaryString.input-DE_Xluxq.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-DX9eJl_F.js";n();var r=e(),i=t.div`
+  min-width: 0;
+`,a=({width:e,longBoth:t})=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{style:{flex:e?`0 0 ${e}px`:t?`1 1 0`:`0 1 auto`},children:`branch slot`})});export{a as App};
