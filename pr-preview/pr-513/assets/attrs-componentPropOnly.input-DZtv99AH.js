45b8c002a1e4cd56ba50c4488cc92161d0ec7a97
skovhus <skovhus@users.noreply.github.com>
2026-06-03 15:25:55 +0000

    Deploy preview for PR 513 🛫

---
+++ pr-preview/pr-513/assets/attrs-componentPropOnly.input-DZtv99AH.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DbhEFSzE.js";import{t as n}from"./text-C1qCCuCf.js";import{t as r}from"./flex-e-79yPuT.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
+  width: 100%;
+  margin-top: 16px;
+`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
