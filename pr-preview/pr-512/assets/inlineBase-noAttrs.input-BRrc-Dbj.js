8e04ac593423d057c87d3f7a372f550ae6f2ccc0
skovhus <skovhus@users.noreply.github.com>
2026-06-03 12:04:22 +0000

    Deploy preview for PR 512 🛫

--- pr-preview/pr-512/assets/inlineBase-noAttrs.input-BRrc-Dbj.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-vjKcU9Ag.js";import{t as n}from"./inline-base-flex-DnxppnUD.js";var r=e(),i=t(n)`
-  padding: 10px;
-  background-color: #eef9ff;
-  border: 1px solid #5aa;
-`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
