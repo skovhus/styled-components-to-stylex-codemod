69a6cb08ac812353cd31e7a4114a25e36a57e4f5
skovhus <skovhus@users.noreply.github.com>
2026-06-04 20:50:00 +0000

    deploy: 04b7507eed20ad32c44d8995328246c90dace3a7

---
+++ assets/wrapper-inline.output-DBDxkKAw.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-DASvrn85.js";import{t as n}from"./external-component-CPLN7jOj.js";t();var r=e();function i(){return(0,r.jsx)(`div`,{children:(0,r.jsx)(n,{isOpen:!0,className:`xdj266r xv8wvd9 xat24cr x1bawvfr`})})}export{i as App};
---
+++ assets/wrapper-styleFnDynamicCallSite.input-DjYaKSMI.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DASvrn85.js";import{t as n}from"./flex-B81UPMb8.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
