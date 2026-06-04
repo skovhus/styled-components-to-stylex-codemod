558f31a3d1233d86c9f6c8688196e3e15b8cf088
skovhus <skovhus@users.noreply.github.com>
2026-06-04 20:53:42 +0000

    Deploy preview for PR 516 🛫

---
+++ pr-preview/pr-516/assets/keyframes-basic.input-Dm2EKMGP.js
@@ -0,0 +1,13 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,s as n}from"./index-Ub7glBWt.js";var r=e(),i=n`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`,a=t.div`
+  display: inline-block;
+  animation: ${i} 2s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+`,o=()=>(0,r.jsx)(a,{children:`💅`});export{o as App};
