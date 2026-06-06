6c47f3a07aa6054e718a25161dcb60332f2d84bf
skovhus <skovhus@users.noreply.github.com>
2026-06-06 20:28:25 +0000

    deploy: 6775e415c5a7273498a7732c232abeb2e3fc433d

---
+++ assets/asProp-crossFile.input-BvD2LVz-.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DwjqaEFZ.js";import{t as n}from"./text-CLzKvGXF.js";var r=e(),i=t(n)`
+  font-size: 24px;
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
---
+++ assets/basic-callExpression.output-91MCfqrJ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-DwjqaEFZ.js";import{A as n}from"./helpers-80fFXPVi.js";t();var r=e(),i=n(e=>(0,r.jsx)(`div`,{...e})),a=()=>(0,r.jsx)(i,{className:`x1e2nbdu`,children:`Hello`});export{a as App};
