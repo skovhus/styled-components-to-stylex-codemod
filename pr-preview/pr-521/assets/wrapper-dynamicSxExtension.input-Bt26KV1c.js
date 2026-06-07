4dc84ce636d0c3415ca38b8085753a574a82c7c0
skovhus <skovhus@users.noreply.github.com>
2026-06-07 16:17:43 +0000

    Deploy preview for PR 521 🛫

---
+++ pr-preview/pr-521/assets/wrapper-dynamicSxExtension.input-Bt26KV1c.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BTlh2DaA.js";import{t as n}from"./sx-aware-component-BFtKRM7u.js";var r=e(),i=t(n)`
+  color: ${e=>`${e.$accentColor??`black`}`};
+`,a=t(i)`
+  margin-left: 4px;
+`,o=()=>(0,r.jsx)(a,{$accentColor:`rgb(20, 60, 90)`,children:`Account`});export{o as App,i as TopContainer};
