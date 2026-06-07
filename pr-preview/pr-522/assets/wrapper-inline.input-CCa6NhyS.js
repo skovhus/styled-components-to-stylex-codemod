2eac3f1ba84c5790953ae0e9cf98491d2a6ddb5a
skovhus <skovhus@users.noreply.github.com>
2026-06-07 18:04:41 +0000

    Deploy preview for PR 522 🛫

---
+++ pr-preview/pr-522/assets/wrapper-inline.input-CCa6NhyS.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-CjF3pd8z.js";import{t as r}from"./external-component-D0gO1zpy.js";n();var i=e(),a=t(r)`
+  margin: 0 -8px 0 -8px;
+`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
