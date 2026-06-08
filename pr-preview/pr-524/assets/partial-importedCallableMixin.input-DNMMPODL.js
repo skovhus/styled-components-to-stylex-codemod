75b5c13a257e4e1a7daf2defb9c550e6c25db2c9
skovhus <skovhus@users.noreply.github.com>
2026-06-08 12:33:50 +0000

    Deploy preview for PR 524 🛫

---
+++ pr-preview/pr-524/assets/partial-importedCallableMixin.input-DNMMPODL.js
@@ -0,0 +1,12 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-D7mEAhiM.js";import{O as n}from"./helpers-D9c243P3.js";var r=e(),i=t.span`
+  ${n()};
+  color: #2563eb;
+  max-width: 120px;
+`,a=t.nav`
+  ${n()};
+  padding: 8px;
+
+  & a.active {
+    color: tomato;
+  }
+`,o=()=>(0,r.jsxs)(a,{children:[(0,r.jsx)(`a`,{className:`active`,href:`#`,children:`Active link`}),(0,r.jsx)(i,{children:`Converted label with long text`})]});export{o as App};
