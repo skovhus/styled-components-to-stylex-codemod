9543a2450a6371d90f9f758308b75f479bea1d0e
skovhus <skovhus@users.noreply.github.com>
2026-06-08 11:50:20 +0000

    Deploy preview for PR 524 🛫

--- pr-preview/pr-524/assets/basic-callExpression.input-Bh6KEqRB.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-B_Vv1UjU.js";import{A as r}from"./helpers-BJZwMOLV.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
-  color: red;
-`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
---
+++ pr-preview/pr-524/assets/selector-crossFileAliased.input-BA2SGTGV.js
@@ -0,0 +1,8 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-ChSSBQFn.js";import{t as n}from"./cross-file-icon.styled-Dpwe2cGP.js";var r=e(),i=t.div`
+  padding: 16px;
+  background-color: #fafafa;
+
+  ${n} {
+    border: 5px solid blue;
+  }
+`;function a(){return(0,r.jsx)(`div`,{style:{padding:16},children:(0,r.jsxs)(i,{children:[(0,r.jsx)(n,{}),`Aliased import`]})})}export{a as App};
