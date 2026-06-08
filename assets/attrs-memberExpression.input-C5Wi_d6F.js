c0380d3d7a493984245ef3d8b25e1834a4d1abbf
skovhus <skovhus@users.noreply.github.com>
2026-06-08 04:50:01 +0000

    deploy: cf30a64337d84cccf04b13ea5aa136b7b34c5932

---
+++ assets/attrs-memberExpression.input-C5Wi_d6F.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-CLZ4rv8L.js";import{t as n}from"./react-spring-wbd3e8hO.js";var r=e(),i=t.div`
+  display: block;
+`,a=t(n.div).attrs({role:`region`})`
+  display: flex;
+  align-items: center;
+`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
