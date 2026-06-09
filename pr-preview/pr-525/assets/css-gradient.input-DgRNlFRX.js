db78d0ef4469062fe7c0f50ebc24f623c50f4acf
skovhus <skovhus@users.noreply.github.com>
2026-06-09 10:32:22 +0000

    Deploy preview for PR 525 🛫

--- pr-preview/pr-525/assets/css-gradient.input-DgRNlFRX.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-NhpBz_cp.js";import{m as n}from"./helpers-Mq1U0Pz1.js";var r=e(),i=t.span`
-  ${n()}
-  font-weight: 600;
-`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
---
+++ pr-preview/pr-525/assets/inlineBase-templateOverride.input-BROsV6F2.js
@@ -0,0 +1,7 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-By22EWxe.js";import{t as n}from"./inline-base-flex-CwPZbHwM.js";var r=e(),i=t(n).attrs({column:!0})`
+  display: grid;
+  flex-direction: row;
+  gap: 4px;
+  padding: 8px;
+  background-color: #eef;
+`;function a(){return(0,r.jsx)(i,{children:`Override`})}export{a as App};
