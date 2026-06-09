f8b5a0bb8010cb83e74b98c7bfdea11736d6e293
skovhus <skovhus@users.noreply.github.com>
2026-06-09 12:40:47 +0000

    Deploy preview for PR 526 🛫

--- pr-preview/pr-526/assets/helper-importNameConflict.input-hyrPjXOM.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-ClHJ3QWI.js";import{D as r}from"./helpers-misizKq8.js";n();var i=e(),a=r(`fast`),o=t.div`
-  transition: color ${r(`normal`)};
-`,s=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`div`,{children:a}),(0,i.jsx)(o,{})]});export{s as App};
