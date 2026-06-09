0b9160e243c6e1d94af811a6ecb85d4baf42239b
skovhus <skovhus@users.noreply.github.com>
2026-06-09 11:23:48 +0000

    Deploy preview for PR 525 🛫

--- pr-preview/pr-525/assets/mixin-extraClassNames.input-B7lf_bLM.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DeIurFBA.js";import{s as n}from"./helpers-VjHJ9ifZ.js";var r=e(),i=t.div`
-  pointer-events: all;
-  ${n(!0)};
-`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
