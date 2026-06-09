c6d9de0b003d63e0b8606b1c610ceab914d267c6
skovhus <skovhus@users.noreply.github.com>
2026-06-09 14:59:49 +0000

    Deploy preview for PR 526 🛫

---
+++ pr-preview/pr-526/assets/basic-callExpression.input-C-B8McvF.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-SuLUS_Xm.js";import{A as r}from"./helpers-7QowWKo8.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
