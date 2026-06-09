607996a9ec1a6ea15590b6a4e2039cd989302a79
skovhus <skovhus@users.noreply.github.com>
2026-06-09 09:47:18 +0000

    Deploy preview for PR 525 🛫

---
+++ pr-preview/pr-525/assets/flex-Bx32nSm9.js
@@ -0,0 +1,1 @@
+import{s as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-C7GR3zXQ.js";var n=e(t(),1);function r(e){let{align:t,gap:r,direction:i,...a}=e;return n.createElement(`div`,a)}export{r as t};
---
+++ pr-preview/pr-525/assets/inlineBase-templateOverride.input-D0xQIyQ-.js
@@ -0,0 +1,7 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-C7GR3zXQ.js";import{t as n}from"./inline-base-flex-C385HNRm.js";var r=e(),i=t(n).attrs({column:!0})`
+  display: grid;
+  flex-direction: row;
+  gap: 4px;
+  padding: 8px;
+  background-color: #eef;
+`;function a(){return(0,r.jsx)(i,{children:`Override`})}export{a as App};
