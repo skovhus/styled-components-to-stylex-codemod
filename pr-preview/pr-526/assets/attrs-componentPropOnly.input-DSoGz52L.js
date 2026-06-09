2ed1d83430ffa4c3ea939e5ab253eb2d9c1830ad
skovhus <skovhus@users.noreply.github.com>
2026-06-09 12:05:22 +0000

    Deploy preview for PR 526 🛫

---
+++ pr-preview/pr-526/assets/attrs-componentPropOnly.input-DSoGz52L.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-B_PjT1X0.js";import{t as n}from"./text-C20_TAaj.js";import{t as r}from"./flex-M_xa1341.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
+  width: 100%;
+  margin-top: 16px;
+`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
---
+++ pr-preview/pr-526/assets/wrapper-inline.output-BW2jh6Ut.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-B_PjT1X0.js";import{t as n}from"./external-component-BM7iepZq.js";t();var r=e();function i(){return(0,r.jsx)(`div`,{children:(0,r.jsx)(n,{isOpen:!0,className:`xdj266r xv8wvd9 xat24cr x1bawvfr`})})}export{i as App};
