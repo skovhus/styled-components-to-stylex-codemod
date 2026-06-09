11a59d4541e59db3cbb727cfc10e45d34c46047d
skovhus <skovhus@users.noreply.github.com>
2026-06-09 18:40:55 +0000

    Deploy preview for PR 526 🛫

--- pr-preview/pr-526/assets/selector-crossFileAliased.input-CB9d9UWc.js
+++
@@ -1,8 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DuuAEP9Q.js";import{t as n}from"./cross-file-icon.styled-BxG-mjB_.js";var r=e(),i=t.div`
-  padding: 16px;
-  background-color: #fafafa;
-
-  ${n} {
-    border: 5px solid blue;
-  }
-`;function a(){return(0,r.jsx)(`div`,{style:{padding:16},children:(0,r.jsxs)(i,{children:[(0,r.jsx)(n,{}),`Aliased import`]})})}export{a as App};
---
+++ pr-preview/pr-526/assets/wrapper-inline.input-D595HgW4.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-donLY8Sn.js";import{t as r}from"./external-component-Dl8abr5i.js";n();var i=e(),a=t(r)`
+  margin: 0 -8px 0 -8px;
+`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
