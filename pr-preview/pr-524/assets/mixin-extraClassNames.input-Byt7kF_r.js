c1c19f23d72ea13077f74c0e4db1dd5b2818bb8c
skovhus <skovhus@users.noreply.github.com>
2026-06-09 08:33:08 +0000

    Deploy preview for PR 524 🛫

---
+++ pr-preview/pr-524/assets/mixin-extraClassNames.input-Byt7kF_r.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-B-n2AEUO.js";import{s as n}from"./helpers-Bi9qITru.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
