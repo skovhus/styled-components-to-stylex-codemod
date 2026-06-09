412ef92cc2fd55ce8268b29a75b41ed66e98c5b1
skovhus <skovhus@users.noreply.github.com>
2026-06-09 11:04:28 +0000

    Deploy preview for PR 525 🛫

--- pr-preview/pr-525/assets/asProp-crossFile.input-CFG08PMQ.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-CRNSQQ_W.js";import{t as n}from"./text-CYtC8yKa.js";var r=e(),i=t(n)`
-  font-size: 24px;
-  font-weight: 600;
-`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
