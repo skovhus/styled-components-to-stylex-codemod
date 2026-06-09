bfb0f9e81d36d7b03531b82558c5338e28b8aa66
skovhus <skovhus@users.noreply.github.com>
2026-06-09 14:38:02 +0000

    Deploy preview for PR 526 🛫

---
+++ pr-preview/pr-526/assets/helper-importNameConflict.input-BGb0WLIU.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-BBeFdd9k.js";import{D as r}from"./helpers-Cqu7-RSD.js";n();var i=e(),a=r(`fast`),o=t.div`
+  transition: color ${r(`normal`)};
+`,s=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`div`,{children:a}),(0,i.jsx)(o,{})]});export{s as App};
--- pr-preview/pr-526/assets/inlineBase-noAttrs.input-Bc9XALL_.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-CdtNEIDC.js";import{t as n}from"./inline-base-flex-Byiqy5_b.js";var r=e(),i=t(n)`
-  padding: 10px;
-  background-color: #eef9ff;
-  border: 1px solid #5aa;
-`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
