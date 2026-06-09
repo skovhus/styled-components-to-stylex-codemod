af222f1c7f07caf6d2428b7c52fe2e3275fe8d2a
skovhus <skovhus@users.noreply.github.com>
2026-06-09 10:57:47 +0000

    Deploy preview for PR 525 🛫

--- pr-preview/pr-525/assets/import-valueNested.input-CpD1MJdW.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-Cm5rDw2V.js";import{o as r}from"./helpers-Dl2Ot2f5.js";n();var i=e(),a=t.div`
-  padding: ${r.ui.spacing.medium};
-  margin: ${r.ui.spacing.small};
-  background: white;
-  border-radius: 8px;
-`;function o(){return(0,i.jsx)(a,{})}export{o as App};
--- pr-preview/pr-525/assets/inlineBase-exportedNoLocalJsx.input-D-uvC6Wl.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-Cm5rDw2V.js";import{t as n}from"./inline-base-flex-BZX7z56b.js";var r=e(),i=t(n).attrs({column:!0,gap:16})`
-  padding: 4px;
-  background-color: #f4f4ff;
-`;function a(){return(0,r.jsx)(`div`,{children:`Exported only`})}export{a as App,i as Container};
