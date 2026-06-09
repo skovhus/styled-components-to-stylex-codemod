c3fe19b96ec885452d803d15f5e55a0c45d2bcbe
skovhus <skovhus@users.noreply.github.com>
2026-06-09 19:27:38 +0000

    Deploy preview for PR 526 🛫

--- pr-preview/pr-526/assets/attrs-componentPropOnly.input-cnn6quTA.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-xgHJTWxc.js";import{t as n}from"./text-Bj3wr6sv.js";import{t as r}from"./flex-CKGiT6C8.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
-  width: 100%;
-  margin-top: 16px;
-`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
