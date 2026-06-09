8706466e97222b4582c31e6f202daf7aa57a9ddc
skovhus <skovhus@users.noreply.github.com>
2026-06-09 14:18:16 +0000

    Deploy preview for PR 526 🛫

--- pr-preview/pr-526/assets/inlineBase-noAttrs.input-Dmz01er_.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-XOqsH33f.js";import{t as n}from"./inline-base-flex-D6gjR0rO.js";var r=e(),i=t(n)`
-  padding: 10px;
-  background-color: #eef9ff;
-  border: 1px solid #5aa;
-`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
---
+++ pr-preview/pr-526/assets/inlineBase-noAttrs.input-ZsGLe8pv.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-7Fp9UCEQ.js";import{t as n}from"./inline-base-flex-Dbt59LEv.js";var r=e(),i=t(n)`
+  padding: 10px;
+  background-color: #eef9ff;
+  border: 1px solid #5aa;
+`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
