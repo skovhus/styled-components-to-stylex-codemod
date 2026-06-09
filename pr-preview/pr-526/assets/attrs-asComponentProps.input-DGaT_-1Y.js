d82305cf3480855cbae7d9d6dbf984afce2c0674
skovhus <skovhus@users.noreply.github.com>
2026-06-09 13:44:43 +0000

    Deploy preview for PR 526 🛫

---
+++ pr-preview/pr-526/assets/attrs-asComponentProps.input-DGaT_-1Y.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-T8q0r7cF.js";import{t as n}from"./flex-CNVUgQhF.js";var r=e(),i=t(t.div``).attrs({as:n,column:!0})`
+  background: white;
+  border-radius: 4px;
+`,a=()=>(0,r.jsxs)(i,{children:[(0,r.jsx)(`div`,{children:`Item 1`}),(0,r.jsx)(`div`,{children:`Item 2`})]});export{a as App};
