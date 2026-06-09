c80cf1ddabecbe8d93ba0566e87604fda320bd14
skovhus <skovhus@users.noreply.github.com>
2026-06-09 13:49:54 +0000

    Deploy preview for PR 526 🛫

--- pr-preview/pr-526/assets/inlineBase-noAttrs.input-LmuKGfiy.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-T8q0r7cF.js";import{t as n}from"./inline-base-flex-YAmCqDAJ.js";var r=e(),i=t(n)`
-  padding: 10px;
-  background-color: #eef9ff;
-  border: 1px solid #5aa;
-`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
