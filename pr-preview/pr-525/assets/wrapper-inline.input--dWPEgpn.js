9f87ac836f252702b73a85a7c7ac7058889f2a57
skovhus <skovhus@users.noreply.github.com>
2026-06-09 08:58:21 +0000

    Deploy preview for PR 525 🛫

---
+++ pr-preview/pr-525/assets/wrapper-inline.input--dWPEgpn.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-DxPGek4M.js";import{t as r}from"./external-component-IX4huc1D.js";n();var i=e(),a=t(r)`
+  margin: 0 -8px 0 -8px;
+`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
