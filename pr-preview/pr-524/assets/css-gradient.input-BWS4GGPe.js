5bc96d38b584961617a2b183374facf9f27540f2
skovhus <skovhus@users.noreply.github.com>
2026-06-10 07:22:33 +0000

    Deploy preview for PR 524 🛫

---
+++ pr-preview/pr-524/assets/css-gradient.input-BWS4GGPe.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BmZALJWx.js";import{m as n}from"./helpers-DcJL1di3.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
--- pr-preview/pr-524/assets/inlineBase-exportedNoLocalJsx.input-CHtHYyBL.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-B-n2AEUO.js";import{t as n}from"./inline-base-flex-BWKY-3jr.js";var r=e(),i=t(n).attrs({column:!0,gap:16})`
-  padding: 4px;
-  background-color: #f4f4ff;
-`;function a(){return(0,r.jsx)(`div`,{children:`Exported only`})}export{a as App,i as Container};
