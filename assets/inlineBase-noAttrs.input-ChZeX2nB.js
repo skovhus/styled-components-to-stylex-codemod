3fd7d6eda0c795066618f3671b7ee28c9f8ccee3
skovhus <skovhus@users.noreply.github.com>
2026-06-10 10:16:49 +0000

    deploy: 8a2c711800748488e81f62f7d9a9b1221e50ffbe

---
+++ assets/inlineBase-noAttrs.input-ChZeX2nB.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BimfYg_M.js";import{t as n}from"./inline-base-flex-D4n1p0Kn.js";var r=e(),i=t(n)`
+  padding: 10px;
+  background-color: #eef9ff;
+  border: 1px solid #5aa;
+`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
---
+++ assets/wrapper-styleFnSpread.input-DrXFcILF.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BimfYg_M.js";import{t as n}from"./external-component-JZ0NAvky.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
