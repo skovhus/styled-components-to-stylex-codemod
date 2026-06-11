85ed454dc9895f19e2ad5ecf16b698a10162c072
skovhus <skovhus@users.noreply.github.com>
2026-06-11 15:04:17 +0000

    Deploy preview for PR 527 🛫

--- pr-preview/pr-527/assets/attrs-componentPropOnly.input-Y2hraM7A.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-8Xc76UAu.js";import{t as n}from"./text-CUvOSFmG.js";import{t as r}from"./flex-B9NLbYvJ.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
-  width: 100%;
-  margin-top: 16px;
-`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
