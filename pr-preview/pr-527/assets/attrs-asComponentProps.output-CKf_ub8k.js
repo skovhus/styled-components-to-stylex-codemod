0477ca1c048f033c3825f91949f5447365e592f1
skovhus <skovhus@users.noreply.github.com>
2026-06-11 17:16:48 +0000

    Deploy preview for PR 527 🛫

---
+++ pr-preview/pr-527/assets/attrs-asComponentProps.output-CKf_ub8k.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-DnnJCoXw.js";import{t as n}from"./flex-BAX0DLiW.js";t();var r=e();function i(e){return(0,r.jsx)(n,{...e,column:!0,className:`x12peec7 x12oqio5`})}var a=()=>(0,r.jsxs)(i,{children:[(0,r.jsx)(`div`,{children:`Item 1`}),(0,r.jsx)(`div`,{children:`Item 2`})]});export{a as App};
