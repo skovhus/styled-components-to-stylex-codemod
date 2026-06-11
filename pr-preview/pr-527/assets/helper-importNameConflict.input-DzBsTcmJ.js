6499f2e76685fe21ff9b503fd968fa4d8dc57a00
skovhus <skovhus@users.noreply.github.com>
2026-06-11 14:56:12 +0000

    Deploy preview for PR 527 🛫

--- pr-preview/pr-527/assets/helper-importNameConflict.input-DzBsTcmJ.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-C_O5vJAe.js";import{D as r}from"./helpers-ChEVY_JV.js";n();var i=e(),a=r(`fast`),o=t.div`
-  transition: color ${r(`normal`)};
-`,s=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`div`,{children:a}),(0,i.jsx)(o,{})]});export{s as App};
--- pr-preview/pr-527/assets/wrapper-inlineStyleFnImported.input-DRtXpiVK.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-C_O5vJAe.js";import{t as n}from"./external-component-fXT3Z07J.js";var r=e(),i=t(n)`
-  color: ${e=>e.$color||`gray`};
-  padding: ${e=>e.$padding||`10px`};
-`,a=t(n)`
-  width: ${e=>e.tabIndex};
-`;function o(){return(0,r.jsxs)(`div`,{children:[(0,r.jsx)(i,{$color:`blue`,$padding:`20px`,isOpen:!0}),(0,r.jsx)(i,{isOpen:!1}),(0,r.jsx)(a,{tabIndex:24,isOpen:!0}),(0,r.jsx)(a,{isOpen:!1})]})}export{o as App};
