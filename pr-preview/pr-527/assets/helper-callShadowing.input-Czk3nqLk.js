f60312e2fd9b9be9e6904884408a46413a90e791
skovhus <skovhus@users.noreply.github.com>
2026-06-11 18:14:01 +0000

    Deploy preview for PR 527 🛫

---
+++ pr-preview/pr-527/assets/helper-callShadowing.input-Czk3nqLk.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-IkuZuIjf.js";import"./helpers-3tVV0v3i.js";n();var r=e();function i(){return t.div`
+    background-color: ${(e=>`#${e}`)(`ff0000`)};
+  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
