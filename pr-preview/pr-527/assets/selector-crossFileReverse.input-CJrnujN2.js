e92942ad74937629c1d8b03e2030573718ce70fc
skovhus <skovhus@users.noreply.github.com>
2026-06-11 13:19:56 +0000

    Deploy preview for PR 527 🛫

---
+++ pr-preview/pr-527/assets/selector-crossFileReverse.input-CJrnujN2.js
@@ -0,0 +1,11 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-Cz85UOL3.js";import{n}from"./cross-file-icon.styled-DEYEEKKj.js";var r=e(),i=t.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-color: gray;
+  transition: background-color 0.25s;
+
+  ${n}:hover & {
+    background-color: rebeccapurple;
+  }
+`;function a(){return(0,r.jsx)(`div`,{style:{padding:16},children:(0,r.jsxs)(n,{href:`#`,children:[(0,r.jsx)(i,{}),`Hover me`]})})}export{a as App};
