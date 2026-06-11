99e5462c20252268eabf685961aadbd7e365ce54
skovhus <skovhus@users.noreply.github.com>
2026-06-11 13:41:58 +0000

    Deploy preview for PR 527 🛫

---
+++ pr-preview/pr-527/assets/basic-callExpression.input-BDhrZhXl.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-CP4DXB5S.js";import{A as r}from"./helpers-L1VYc0rR.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
---
+++ pr-preview/pr-527/assets/basic-callExpression.output-G6oZQ4h9.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-CP4DXB5S.js";import{A as n}from"./helpers-L1VYc0rR.js";t();var r=e(),i=n(e=>(0,r.jsx)(`div`,{...e})),a=()=>(0,r.jsx)(i,{className:`x1e2nbdu`,children:`Hello`});export{a as App};
