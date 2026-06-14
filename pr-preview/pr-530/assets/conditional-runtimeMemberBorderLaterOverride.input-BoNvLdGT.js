29e7cba60e2cd5befdccafb4ffadfee1655475e0
skovhus <skovhus@users.noreply.github.com>
2026-06-14 20:43:21 +0000

    Deploy preview for PR 530 🛫

---
+++ pr-preview/pr-530/assets/conditional-runtimeMemberBorderLaterOverride.input-BoNvLdGT.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-BgIyL5_t.js";import{t as n}from"./helpers-DErdaVnJ.js";var r=e(),i=t.div`
+  border: ${n.isTouchDevice?1:2}px solid red;
+  border-top-width: 0;
+  background-color: peachpuff;
+`,a=()=>(0,r.jsx)(i,{children:`Later border-top-width wins`});export{a as App};
