27e1870dd046e99cb7da641c7065897660333fdb
skovhus <skovhus@users.noreply.github.com>
2026-06-14 20:31:44 +0000

    deploy: f013f4c485afe4121177f4e0eb14492cc1cfe31d

---
+++ assets/basic-callExpression.input-B6TuKhkw.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-dszo0YGQ.js";import{A as r}from"./helpers--ViTD21b.js";n();var i=e(),a=t(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
---
+++ assets/import-valueNested.input-CrW7YNjT.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t,p as n}from"./index-dszo0YGQ.js";import{o as r}from"./helpers--ViTD21b.js";n();var i=e(),a=t.div`
+  padding: ${r.ui.spacing.medium};
+  margin: ${r.ui.spacing.small};
+  background: white;
+  border-radius: 8px;
+`;function o(){return(0,i.jsx)(a,{})}export{o as App};
