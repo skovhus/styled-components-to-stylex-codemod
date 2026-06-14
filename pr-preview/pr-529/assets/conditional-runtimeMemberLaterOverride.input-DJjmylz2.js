fbba9894e58d82e567fbb9f25ce3fceb0e4a09ce
skovhus <skovhus@users.noreply.github.com>
2026-06-14 18:04:47 +0000

    Deploy preview for PR 529 🛫

--- pr-preview/pr-529/assets/conditional-runtimeMemberLaterOverride.input-DJjmylz2.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DVxuV9v5.js";import{t as n}from"./helpers-ReMz_9tK.js";var r=e(),i=t.div`
-  margin: ${n.isTouchDevice?4:8}px 12px;
-  margin-top: 0;
-  background-color: peachpuff;
-`,a=()=>(0,r.jsx)(i,{children:`Later margin-top wins`});export{a as App};
