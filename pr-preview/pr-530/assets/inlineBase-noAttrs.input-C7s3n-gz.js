fc7ec1841866295fa1115121d044764262029a64
skovhus <skovhus@users.noreply.github.com>
2026-06-14 20:28:30 +0000

    Deploy preview for PR 530 🛫

---
+++ pr-preview/pr-530/assets/inlineBase-noAttrs.input-C7s3n-gz.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-DZPNHPPk.js";import{t as n}from"./inline-base-flex-iS42rRyC.js";var r=e(),i=t(n)`
+  padding: 10px;
+  background-color: #eef9ff;
+  border: 1px solid #5aa;
+`;function a(){return(0,r.jsx)(i,{children:`No attrs`})}export{a as App};
