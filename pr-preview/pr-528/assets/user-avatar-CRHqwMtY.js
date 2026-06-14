0d3375806818b53547d47a03a26ecbc47f4f9948
skovhus <skovhus@users.noreply.github.com>
2026-06-14 17:11:01 +0000

    Deploy preview for PR 528 🛫

---
+++ pr-preview/pr-528/assets/user-avatar-CRHqwMtY.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{p as t}from"./index-xbWgmne-.js";t();var n=e();function r({className:e,style:t,size:r=`regular`,user:i}){return(0,n.jsx)(`div`,{className:e,style:t,children:(0,n.jsxs)(`span`,{children:[i,` (`,r,`)`]})})}export{r as t};
