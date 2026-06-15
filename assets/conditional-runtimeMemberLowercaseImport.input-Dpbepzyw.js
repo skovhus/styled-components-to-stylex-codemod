695d867ef70efcf5c64b9cf01cfaacfff1a6570d
skovhus <skovhus@users.noreply.github.com>
2026-06-15 09:05:21 +0000

    deploy: b17552870f2ff7c5dfe39d328b262540087bf81b

---
+++ assets/conditional-runtimeMemberLowercaseImport.input-Dpbepzyw.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-B8sTdNyf.js";import{l as t}from"./index-zqQtS4eF.js";import{t as n}from"./helpers-CNXiiB64.js";var r=e(),i=t.div`
+  position: relative;
+  top: ${n.isTouchDevice?5:1}px;
+  background-color: peachpuff;
+`,a=()=>(0,r.jsx)(i,{children:`Lowercase imported runtime condition`});export{a as App};
