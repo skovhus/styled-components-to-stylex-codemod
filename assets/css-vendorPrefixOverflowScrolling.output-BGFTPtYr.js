e9f6c2c743e22f3140e73f47a7e89688dc00fa5b
skovhus <skovhus@users.noreply.github.com>
2026-06-16 15:52:32 +0000

    deploy: 4825d4ca18286dd33792c39c42b57a873c15c5d0

---
+++ assets/css-vendorPrefixOverflowScrolling.output-BGFTPtYr.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";var t=e(),n=()=>(0,t.jsxs)(`div`,{className:`x5lxg6s x1odjw0f x1n4lwwq xe8ttls xmftpkk`,children:[(0,t.jsx)(`div`,{children:`Scrollable panel`}),(0,t.jsx)(`div`,{children:`Second row`})]});export{n as App};
