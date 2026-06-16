a5a65412efc8361849f9a4946b316f21efdf6a2e
skovhus <skovhus@users.noreply.github.com>
2026-06-16 21:05:56 +0000

    Deploy preview for PR 534 🛫

---
+++ pr-preview/pr-534/assets/css-gradient.input-DlSaNy78.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-Fuh1VcKb.js";import{m as n}from"./helpers-C5T1GFcU.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
---
+++ pr-preview/pr-534/assets/useTheme-isDark.input-BoIT5p0r.js
@@ -0,0 +1,11 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-Fuh1VcKb.js";import{E as r}from"./helpers-C5T1GFcU.js";t();var i=e(),a=n.span`
+  font-size: 12px;
+  color: ${e=>e.theme.isDark?e.theme.color.labelBase:e.theme.color.labelMuted};
+  border-color: ${e=>e.theme.isDark?e.theme.color.bgSub:e.theme.color.bgBorderFaint};
+`,o=n.div`
+  ${e=>e.theme.isDark?``:`padding: ${r()};`}
+`,s=n.div`
+  --highlighted-color: ${e=>e.theme.isDark?e.theme.color?.bgBorderSolid:e.theme.color?.bgBorderFaint};
+  background-color: var(--highlighted-color);
+  padding: 16px;
+`,c=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(a,{children:`Label`}),(0,i.jsx)(o,{children:`Box`}),(0,i.jsx)(s,{children:`DayPicker`})]});export{c as App};
