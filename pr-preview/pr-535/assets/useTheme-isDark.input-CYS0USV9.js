2ec29d479f653a1cc3acfd5511b97108325afb0f
skovhus <skovhus@users.noreply.github.com>
2026-06-16 20:33:00 +0000

    Deploy preview for PR 535 🛫

---
+++ pr-preview/pr-535/assets/useTheme-isDark.input-CYS0USV9.js
@@ -0,0 +1,15 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-BZQb850I.js";import{E as r,a as i}from"./helpers-B61StXQj.js";t();var a=e(),o=n.span`
+  font-size: 12px;
+  color: ${e=>e.theme.isDark?e.theme.color.labelBase:e.theme.color.labelMuted};
+  border-color: ${e=>e.theme.isDark?e.theme.color.bgSub:e.theme.color.bgBorderFaint};
+`,s=n.div`
+  background: ${e=>e.theme.isDark?i(`bgBorderSolid`)(e):i(`bgBaseHover`)(e)};
+  color: ${i(`labelBase`)};
+  padding: 12px;
+`,c=n.div`
+  ${e=>e.theme.isDark?``:`padding: ${r()};`}
+`,l=n.div`
+  --highlighted-color: ${e=>e.theme.isDark?e.theme.color?.bgBorderSolid:e.theme.color?.bgBorderFaint};
+  background-color: var(--highlighted-color);
+  padding: 16px;
+`,u=()=>(0,a.jsxs)(`div`,{children:[(0,a.jsx)(o,{children:`Label`}),(0,a.jsx)(s,{children:`Helper color box`}),(0,a.jsx)(c,{children:`Box`}),(0,a.jsx)(l,{children:`DayPicker`})]});export{u as App};
