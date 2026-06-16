9747ff664853fbe1262932251cb3741115e4599b
skovhus <skovhus@users.noreply.github.com>
2026-06-16 18:07:34 +0000

    Deploy preview for PR 532 🛫

--- pr-preview/pr-532/assets/asProp-crossFile.input-EpIe7K9J.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-Epins4WS.js";import{t as n}from"./text-CWQRxhqo.js";var r=e(),i=t(n)`
-  font-size: 24px;
-  font-weight: 600;
-`,a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
