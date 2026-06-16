1602859e57990e70996a6a526c63943f1b2aa215
skovhus <skovhus@users.noreply.github.com>
2026-06-16 16:41:32 +0000

    Deploy preview for PR 532 🛫

--- pr-preview/pr-532/assets/basic-callExpression.output-DVIth4mx.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t}from"./index-C9mqQtPz.js";import{A as n}from"./helpers-B7eZQ8U1.js";t();var r=e(),i=n(e=>(0,r.jsx)(`div`,{...e})),a=()=>(0,r.jsx)(i,{className:`x1e2nbdu`,children:`Hello`});export{a as App};
---
+++ pr-preview/pr-532/assets/wrapper-styleFnSpread.input-C3pwXm07.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-CeT4uIzl.js";import{t as n}from"./external-component-jnpVUwGk.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
