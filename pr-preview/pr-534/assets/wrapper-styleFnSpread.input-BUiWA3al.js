f74c6d040caf569d4f08d663b6b590e91b7a486a
skovhus <skovhus@users.noreply.github.com>
2026-06-16 21:20:52 +0000

    Deploy preview for PR 534 🛫

---
+++ pr-preview/pr-534/assets/wrapper-styleFnSpread.input-BUiWA3al.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-BP6N_hhQ.js";import{t as n}from"./external-component-Dusu8jHt.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
