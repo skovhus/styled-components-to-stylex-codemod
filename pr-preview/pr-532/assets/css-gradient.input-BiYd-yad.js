db4652b860d14d2dfb3a6ab3a7279afeabe4e42c
skovhus <skovhus@users.noreply.github.com>
2026-06-16 17:32:35 +0000

    Deploy preview for PR 532 🛫

---
+++ pr-preview/pr-532/assets/css-gradient.input-BiYd-yad.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-Epins4WS.js";import{m as n}from"./helpers-BSlf_QTb.js";var r=e(),i=t.span`
+  ${n()}
+  font-weight: 600;
+`,a=()=>(0,r.jsx)(i,{children:`Gradient text`});export{a as App,i as GradientText};
