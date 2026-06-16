205a4aa016902510de896eb6e12b1744ecc1adf3
skovhus <skovhus@users.noreply.github.com>
2026-06-16 21:49:20 +0000

    Deploy preview for PR 534 🛫

--- pr-preview/pr-534/assets/helper-callShadowing.input-GfnMRB0e.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-Do0IHkV6.js";import"./helpers-J683zR6_.js";t();var r=e();function i(){return n.div`
-    background-color: ${(e=>`#${e}`)(`ff0000`)};
-  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
--- pr-preview/pr-534/assets/mixin-extraClassNames.input-sE5q-7aG.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-Do0IHkV6.js";import{s as n}from"./helpers-J683zR6_.js";var r=e(),i=t.div`
-  pointer-events: all;
-  ${n(!0)};
-`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
