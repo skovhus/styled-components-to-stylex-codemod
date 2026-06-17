a9a0d1294e559e8a5ac966e935b4c2af85b7976f
skovhus <skovhus@users.noreply.github.com>
2026-06-17 04:34:02 +0000

    Deploy preview for PR 534 🛫

--- pr-preview/pr-534/assets/asProp-crossFile.output-CsLm7tSl.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t}from"./index-B_JHf2AF.js";import{t as n}from"./text-5a7Aur7z.js";t();var r=e();function i(e){let{as:t=n,...i}=e;return(0,r.jsx)(t,{...i,className:`x1pvqxga x1s688f`})}var a=()=>(0,r.jsx)(`div`,{children:(0,r.jsx)(i,{variant:`large`,children:`Default Title`})});export{a as App,i as HeaderTitle};
--- pr-preview/pr-534/assets/mixin-extraClassNames.input-CawZ92zu.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-B_JHf2AF.js";import{s as n}from"./helpers-B0dxdV9T.js";var r=e(),i=t.div`
-  pointer-events: all;
-  ${n(!0)};
-`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
