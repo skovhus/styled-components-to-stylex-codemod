1f40db8a5aa2ecbd964c651fcaced92e162ecb68
skovhus <skovhus@users.noreply.github.com>
2026-06-17 01:09:07 +0000

    Deploy preview for PR 534 🛫

---
+++ pr-preview/pr-534/assets/extending-chained.input-BhMGXiJI.js
@@ -0,0 +1,7 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-EkCQVWtM.js";import{t as r}from"./text-D7itz7ob.js";t();var i=e(),a=n(n(r)`
+  margin-left: 8px;
+`)`
+  margin-left: 4px;
+`,o=n(r)`
+  margin-right: 4px;
+`,s=()=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(a,{children:`Help text content`}),(0,i.jsx)(o,{children:`|`})]});export{s as App,a as HelpText,o as Separator};
