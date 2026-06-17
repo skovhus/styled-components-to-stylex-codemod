c14f2516510d8550d9da58507a656173d1b33439
skovhus <skovhus@users.noreply.github.com>
2026-06-17 06:49:26 +0000

    Deploy preview for PR 535 🛫

--- pr-preview/pr-535/assets/wrapper-styleFnDynamicCallSite.input-DeM_m_kt.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-owLqLUoK.js";import{t as n}from"./flex-Cph7jkBQ.js";var r=e(),i=t(n)`
-  margin-top: ${e=>(e.windowHeight-400)/2}px;
-`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
