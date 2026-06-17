4b55a5b74d86e2e15f2988e58edbac2dde0d5322
skovhus <skovhus@users.noreply.github.com>
2026-06-17 08:42:03 +0000

    Deploy preview for PR 535 🛫

---
+++ pr-preview/pr-535/assets/basic-callExpression.input-CbnckVLa.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-zs9G4pn0.js";import{j as r}from"./helpers-CYXjNn2I.js";t();var i=e(),a=n(r(e=>(0,i.jsx)(`div`,{...e})))`
+  color: red;
+`,o=()=>(0,i.jsx)(a,{children:`Hello`});export{o as App};
---
+++ pr-preview/pr-535/assets/wrapper-styleFnSpread.input-DTYnLhk2.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-zs9G4pn0.js";import{t as n}from"./external-component-t19yGXmo.js";var r=e(),i=t(n)`
+  color: ${e=>e.$color||`gray`};
+  padding: 10px;
+`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
