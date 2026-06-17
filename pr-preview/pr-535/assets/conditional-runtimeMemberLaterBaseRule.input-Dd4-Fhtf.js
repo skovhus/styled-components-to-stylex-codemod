27ea6a4686d578920408b33aca7420decd53dcc6
skovhus <skovhus@users.noreply.github.com>
2026-06-17 07:47:14 +0000

    Deploy preview for PR 535 🛫

--- pr-preview/pr-535/assets/conditional-runtimeMemberLaterBaseRule.input-Dd4-Fhtf.js
+++
@@ -1,8 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-D2qi_qk5.js";import{t as n}from"./helpers-DIyCQ7CB.js";var r=e(),i=t.div`
-  position: relative;
-  top: ${n.isTouchDevice?5:1}px;
-  & {
-    top: 2px;
-  }
-  background-color: peachpuff;
-`,a=()=>(0,r.jsx)(i,{children:`Later base rule wins`});export{a as App};
