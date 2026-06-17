e6cc43d12209e2517339b3b1deac3cbbe32528ca
skovhus <skovhus@users.noreply.github.com>
2026-06-17 02:32:18 +0000

    Deploy preview for PR 534 🛫

--- pr-preview/pr-534/assets/helper-callShadowing.input-DQr4JV3l.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-D-SG0Ub4.js";import"./helpers-Bnw1tqmx.js";t();var r=e();function i(){return n.div`
-    background-color: ${(e=>`#${e}`)(`ff0000`)};
-  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
