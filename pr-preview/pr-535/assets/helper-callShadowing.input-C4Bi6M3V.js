3c15bc3b3e4e4d67be1cc82f511a0d7a781b274f
skovhus <skovhus@users.noreply.github.com>
2026-06-17 08:22:44 +0000

    Deploy preview for PR 535 🛫

--- pr-preview/pr-535/assets/helper-callShadowing.input-C4Bi6M3V.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-BT0-fJb3.js";import"./helpers-B17A-pbB.js";t();var r=e();function i(){return n.div`
-    background-color: ${(e=>`#${e}`)(`ff0000`)};
-  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
