564208ee05ae2831479593da60631af526e4e375
skovhus <skovhus@users.noreply.github.com>
2026-06-17 09:03:23 +0000

    Deploy preview for PR 536 🛫

---
+++ pr-preview/pr-536/assets/wrapper-dynamicSxExtension.input-9O7b-xfe.js
@@ -0,0 +1,5 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-CDHI0b0X.js";import{t as n}from"./sx-aware-component-CagG0_Qz.js";var r=e(),i=t(n)`
+  color: ${e=>`${e.$accentColor??`black`}`};
+`,a=t(i)`
+  margin-left: 4px;
+`,o=()=>(0,r.jsx)(a,{$accentColor:`rgb(20, 60, 90)`,children:`Account`});export{o as App,i as TopContainer};
--- pr-preview/pr-536/assets/wrapper-inline.input-l3kW2hId.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-CskPFWP8.js";import{t as r}from"./external-component-Cq4ENzc7.js";t();var i=e(),a=n(r)`
-  margin: 0 -8px 0 -8px;
-`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
