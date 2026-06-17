c423ae7a6fa16f69a9ac46ebc1697a3ca48e5b4a
skovhus <skovhus@users.noreply.github.com>
2026-06-17 08:07:51 +0000

    Deploy preview for PR 536 🛫

---
+++ pr-preview/pr-536/assets/attrs-componentPropOnly.input-aIJNg56c.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-DIW23hD4.js";import{t as n}from"./text-Ct_w0vFn.js";import{t as r}from"./flex-CFroBpWR.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
+  width: 100%;
+  margin-top: 16px;
+`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
