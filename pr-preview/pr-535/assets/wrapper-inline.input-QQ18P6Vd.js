6f3db354c985d58ad08e156bee637b6e67434760
skovhus <skovhus@users.noreply.github.com>
2026-06-17 09:22:22 +0000

    Deploy preview for PR 535 🛫

--- pr-preview/pr-535/assets/wrapper-inline.input-QQ18P6Vd.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-Dp0z9M9W.js";import{t as r}from"./external-component-B9Cqskho.js";t();var i=e(),a=n(r)`
-  margin: 0 -8px 0 -8px;
-`;function o(){return(0,i.jsx)(`div`,{children:(0,i.jsx)(a,{isOpen:!0})})}export{o as App};
