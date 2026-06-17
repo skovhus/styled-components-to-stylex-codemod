4495fb1daa64449395f4a48d0d9b6f8a5c071723
skovhus <skovhus@users.noreply.github.com>
2026-06-17 07:22:15 +0000

    Deploy preview for PR 535 🛫

--- pr-preview/pr-535/assets/attrs-componentPropOnly.input-BFJf2k5H.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-biRCDt2B.js";import{t as n}from"./text-D65hswMZ.js";import{t as r}from"./flex-BcVbhky4.js";var i=e(),a=t(n).attrs({variant:`title2`})``,o=t(r).attrs({column:!0,gap:16,align:`center`})`
-  width: 100%;
-  margin-top: 16px;
-`,s=()=>(0,i.jsxs)(`div`,{style:{padding:`16px`},children:[(0,i.jsx)(a,{children:`Hello World`}),(0,i.jsx)(o,{children:(0,i.jsx)(`span`,{children:`Something went wrong`})})]});export{s as App};
--- pr-preview/pr-535/assets/attrs-memberExpression.input-hCMhStJY.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-biRCDt2B.js";import{t as n}from"./react-spring-DEsb3H-H.js";var r=e(),i=t.div`
-  display: block;
-`,a=t(n.div).attrs({role:`region`})`
-  display: flex;
-  align-items: center;
-`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
