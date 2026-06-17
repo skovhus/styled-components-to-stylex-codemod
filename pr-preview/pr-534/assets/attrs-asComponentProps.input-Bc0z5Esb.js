d94809f9c5a4f7e2ab3b27dbc5009a19e75847d6
skovhus <skovhus@users.noreply.github.com>
2026-06-17 01:21:24 +0000

    Deploy preview for PR 534 🛫

---
+++ pr-preview/pr-534/assets/attrs-asComponentProps.input-Bc0z5Esb.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-UsIi0MPL.js";import{t as n}from"./flex-00mn3n9c.js";var r=e(),i=t(t.div``).attrs({as:n,column:!0})`
+  background: white;
+  border-radius: 4px;
+`,a=()=>(0,r.jsxs)(i,{children:[(0,r.jsx)(`div`,{children:`Item 1`}),(0,r.jsx)(`div`,{children:`Item 2`})]});export{a as App};
