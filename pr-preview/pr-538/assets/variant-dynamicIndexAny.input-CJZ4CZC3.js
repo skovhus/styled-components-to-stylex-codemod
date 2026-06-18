42fe1627cc0415905ea1c30188bd465178d26a5a
skovhus <skovhus@users.noreply.github.com>
2026-06-18 22:06:03 +0000

    Deploy preview for PR 538 🛫

---
+++ pr-preview/pr-538/assets/variant-dynamicIndexAny.input-CJZ4CZC3.js
@@ -0,0 +1,17 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-DPPpnsv9.js";var n=e(),r=t(`div`)`
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  flex-shrink: 0;
+  background: ${e=>e.color||`gray`};
+
+  ${e=>e.size===`tiny`&&`
+    width: 7px;
+    height: 7px;
+  `};
+
+  ${e=>e.size===`small`&&`
+    width: 9px;
+    height: 9px;
+  `};
+`,i=()=>(0,n.jsxs)(`div`,{style:{display:`flex`,gap:8},children:[(0,n.jsx)(r,{color:`red`,size:`tiny`}),(0,n.jsx)(r,{color:`blue`,size:`small`}),(0,n.jsx)(r,{color:`green`}),(0,n.jsx)(r,{})]});export{i as App,r as Badge};
