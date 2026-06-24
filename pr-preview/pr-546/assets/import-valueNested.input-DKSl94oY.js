c33dfb5003e7eb7f21257f24c6526241cf62dfb0
skovhus <skovhus@users.noreply.github.com>
2026-06-24 04:56:32 +0000

    Deploy preview for PR 546 🛫

---
+++ pr-preview/pr-546/assets/import-valueNested.input-DKSl94oY.js
@@ -0,0 +1,6 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-CU8qmYyO.js";import{o as r}from"./helpers-C01zpmZK.js";t();var i=e(),a=n.div`
+  padding: ${r.ui.spacing.medium};
+  margin: ${r.ui.spacing.small};
+  background: white;
+  border-radius: 8px;
+`;function o(){return(0,i.jsx)(a,{})}export{o as App};
---
+++ pr-preview/pr-546/assets/wrapper-delegation.input-Dgq3rH0J.js
@@ -0,0 +1,8 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-CU8qmYyO.js";import{a as n}from"./helpers-C01zpmZK.js";var r=e();function i(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(a,{children:`Test`}),(0,r.jsx)(s,{style:{marginBottom:0},children:`Test`}),(0,r.jsx)(o,{children:`Okay`})]})}i.displayName=`App`;var a=t.div`
+  text-align: center;
+  margin-bottom: 32px;
+`,o=t(a)`
+  padding: 0 32px;
+`,s=t(o)`
+  color: ${n(`labelMuted`)};
+`;export{i as App};
