d6cd184c2812efe3e040772e53fa0dd07782dc82
skovhus <skovhus@users.noreply.github.com>
2026-06-29 09:42:01 +0000

    Deploy preview for PR 548 🛫

--- pr-preview/pr-548/assets/attrs-memberExpression.input-De--vKD4.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-CKlG2ZVw.js";import{t as n}from"./react-spring-Cj2zX8CT.js";var r=e(),i=t.div`
-  display: block;
-`,a=t(n.div).attrs({role:`region`})`
-  display: flex;
-  align-items: center;
-`,o=()=>(0,r.jsx)(i,{children:(0,r.jsx)(a,{children:`Hello`})});export{o as App};
