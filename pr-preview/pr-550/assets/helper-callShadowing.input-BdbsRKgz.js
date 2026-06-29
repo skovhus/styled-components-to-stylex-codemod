7f3c0ef479d091592c6238f708231288eb21d853
skovhus <skovhus@users.noreply.github.com>
2026-06-29 12:50:39 +0000

    Deploy preview for PR 550 🛫

--- pr-preview/pr-550/assets/helper-callShadowing.input-BdbsRKgz.js
+++
@@ -1,3 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{m as t,u as n}from"./index-BrCQKIJn.js";import"./helpers-s8kspoFP.js";t();var r=e();function i(){return n.div`
-    background-color: ${(e=>`#${e}`)(`ff0000`)};
-  `}var a=()=>(0,r.jsx)(i(),{children:`Colored text`});export{a as App};
--- pr-preview/pr-550/assets/wrapper-styleFnSpread.input-1fXx_I5_.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-BrCQKIJn.js";import{t as n}from"./external-component-DAyYDprW.js";var r=e(),i=t(n)`
-  color: ${e=>e.$color||`gray`};
-  padding: 10px;
-`;function a(e){return(0,r.jsx)(i,{...e})}export{a as App};
