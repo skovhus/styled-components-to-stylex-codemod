2408cf063fdf2a6b1f57652b8e8f6bd6d3e98dba
skovhus <skovhus@users.noreply.github.com>
2026-06-29 18:10:17 +0000

    deploy: c335310cbe887dc4889a8e61cf43ba3c6abad8df

---
+++ assets/cssHelper-reuse.input-CJijyF3t.js
@@ -0,0 +1,28 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{l as t,u as n}from"./index-BbiUvuS5.js";var r=e(),i=t`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-column: 1 / -1;
+  align-items: center;
+  padding: 0 8px;
+  min-height: 36px;
+  background: ${({theme:e})=>e.color.bgBase};
+`,a=n.div`
+  ${i}
+  position: sticky;
+  top: var(--sticky-top, 0px);
+  z-index: 3; /* above regular rows */
+  border-top: 1px solid ${({theme:e})=>e.color.bgBorderFaint};
+  border-bottom: 1px solid ${({theme:e})=>e.color.bgBorderFaint};
+`,o=n.div`
+  ${i}
+  &:hover {
+    background: ${({theme:e})=>e.color.bgBaseHover};
+  }
+`,s=n.div`
+  ${e=>e.$opaque&&t`
+      opacity: 0.4;
+    `}
+  width: 10px;
+  height: 10px;
+  background-color: red;
+`,c=()=>(0,r.jsxs)(`div`,{children:[(0,r.jsx)(a,{children:`Group`}),(0,r.jsx)(o,{children:`Project`}),(0,r.jsx)(s,{$opaque:!0}),(0,r.jsx)(s,{$opaque:!1})]});export{c as App};
---
+++ assets/wrapper-styleFnDynamicCallSite.input-DwchkhGn.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-BbiUvuS5.js";import{t as n}from"./flex-d9DMm21k.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
