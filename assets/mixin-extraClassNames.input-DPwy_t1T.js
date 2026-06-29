5d5519f33f965e79a98ce739be9e574345bf0d5e
skovhus <skovhus@users.noreply.github.com>
2026-06-29 09:06:07 +0000

    deploy: f91a511979135548ee8351c05348990ef29a59d3

---
+++ assets/mixin-extraClassNames.input-DPwy_t1T.js
@@ -0,0 +1,4 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-BXL_OpXW.js";import{s as n}from"./helpers-B2HaGnZm.js";var r=e(),i=t.div`
+  pointer-events: all;
+  ${n(!0)};
+`;function a(){return(0,r.jsx)(i,{children:`Draggable`})}export{a as App};
