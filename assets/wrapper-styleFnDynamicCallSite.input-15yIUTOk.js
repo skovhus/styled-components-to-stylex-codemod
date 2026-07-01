ceccac3296a550f0012b9eef51a1021e0c93d7a8
skovhus <skovhus@users.noreply.github.com>
2026-07-01 10:57:45 +0000

    deploy: a2d285484b284f5d7df58525e33183091c0637c2

---
+++ assets/wrapper-styleFnDynamicCallSite.input-15yIUTOk.js
@@ -0,0 +1,3 @@
+import{t as e}from"./jsx-runtime-D4ePz0Hl.js";import{u as t}from"./index-CJRCtd4b.js";import{t as n}from"./flex-De-19RWc.js";var r=e(),i=t(n)`
+  margin-top: ${e=>(e.windowHeight-400)/2}px;
+`,a=e=>(0,r.jsx)(i,{windowHeight:e.windowHeight,children:`Content`});export{a as App};
